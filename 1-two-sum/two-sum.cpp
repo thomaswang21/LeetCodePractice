8ac53277dd4e8700b8dcb576db1ebf768d53993c
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-09-12 03:14:39 -0700

    Time: 8 ms (43.92%) | Memory: 16 MB (6.09%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>hash;
+        for(int i=0; i<nums.size();i++){
+            hash[nums[i]]=i;
+        }
+        for(int i=0;i<nums.size();i++){
+            int num=target-nums[i];
+            if(hash.count(num)&&hash[num]!=i){
+                return {i, hash[num]};
+            }
+        }
+        return {};
+    }
+};
