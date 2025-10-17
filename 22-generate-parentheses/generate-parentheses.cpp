0a5c2338fc7cee2b47b26223925a38c238141d93
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-17 14:31:44 -0700

    Time: 0 ms (100.00%) | Memory: 13.3 MB (75.12%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,21 +1,39 @@
 class Solution {
+private:
+    // 回溯过程中的路径
+    string track = "";
+    // 记录所有合法的括号组合
+    vector<string> res; 
 public:
-    vector<string>ans;
-
     vector<string> generateParenthesis(int n) {
-        dfs(n,0,0,"");
-        return ans;    
+        if (n == 0) return res;
+        // 可用的左括号和右括号数量初始化为 n
+        backtrack(n, n);
+        return res;
     }
-    void dfs(int n, int lc, int rc, string sec){
-        if(lc==n&&rc==n){
-            ans.push_back(sec);
+
+    // 可用的左括号数量为 left 个，可用的右括号数量为 right 个
+    void backtrack(int left, int right) {
+        // 若左括号剩下的多，说明不合法
+        if (right < left) return;
+        // 数量小于 0 肯定是不合法的
+        if (left < 0 || right < 0) return;
+        // 当所有括号都恰好用完时，得到一个合法的括号组合
+        if (left == 0 && right == 0) {
+            res.push_back(track);
             return;
         }
-        if(lc<n){
-            dfs(n, lc+1, rc,sec+'(');
-        }
-        if(rc<n&&rc<lc){
-            dfs(n, lc, rc+1, sec+')');
-        }
+
+        // 做选择，尝试放一个左括号
+        track.push_back('(');
+        backtrack(left - 1, right);
+        // 撤消选择
+        track.pop_back();
+
+        // 做选择，尝试放一个右括号
+        track.push_back(')');
+        backtrack(left, right - 1);
+        // 撤销选择
+        track.pop_back();
     }
 };
