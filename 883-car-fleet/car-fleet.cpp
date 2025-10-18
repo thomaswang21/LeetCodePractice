9439cf902d2a30d27347dfaa8806fe6576c316e4
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-17 17:21:27 -0700

    Time: 135 ms (14.12%) | Memory: 128.9 MB (6.22%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,37 @@
+
+
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<vector<int>> cars(n, vector<int>(2));
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        // 按照初始位置，从小到大排序
+        sort(cars.begin(), cars.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        
+        // 计算每辆车到达终点的时间
+        vector<double> time(n);
+        for (int i = 0; i < n; i++) {
+            int pos = cars[i][0];
+            int vel = cars[i][1];
+            time[i] = (double)(target - pos) / vel;
+        }
+        
+        // 使用单调栈计算车队的数量
+        // 避免使用栈模拟，倒序遍历取递增序列就是答案
+        int res = 0;
+        double maxTime = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (time[i] > maxTime) {
+                maxTime = time[i];
+                res++;
+            }
+        }
+        return res;
+    }
+};
