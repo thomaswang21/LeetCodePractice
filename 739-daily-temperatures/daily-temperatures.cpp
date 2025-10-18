bb44ac95a0f6f4367b0491d9183a3b0d722e0da2
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-17 17:03:59 -0700

    Time: 19 ms (72.28%) | Memory: 102.8 MB (79.80%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> res(n, 0);
-        // 这里放元素索引，而不是元素
-        stack<int> s;
-        // 单调栈模板
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.empty() && temperatures[s.top()] <= temperatures[i]) {
+        int n=temperatures.size();
+        stack<int>s;
+        vector<int>res(n,0);
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty()&&temperatures[s.top()]<=temperatures[i]){
                 s.pop();
             }
-            // 得到索引间距
-            if (s.empty()) {
-                res[i] = 0;
-            } else {
-                res[i] = s.top() - i;
+            if(s.empty()){
+                res[i]=0;
+            }else{
+                res[i]=s.top()-i;
             }
-            // 将索引入栈，而不是元素
-            s.push(i); 
+            s.push(i);
         }
         return res;
     }
