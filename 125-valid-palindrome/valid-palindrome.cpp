1f926c69aa10845473de2dcbce1cc0d00b97d48d
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-19 01:59:00 -0700

    Time: 0 ms (100.00%) | Memory: 10.1 MB (41.53%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string sb;
+        for(int i=0;i<s.size();i++){
+            char c=s[i];
+            if(isalnum(c)){
+                sb+=tolower(c);
+            }
+        }
+        s=sb;
+        int left=0, right=s.size()-1;
+        while(left<right){
+            if(s[left]!=s[right])return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
