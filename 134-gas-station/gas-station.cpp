04529c7155fc82ebd6d05558ef4f9dad5740fcab
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-21 20:47:34 -0700

    Time: 0 ms (100.00%) | Memory: 112.3 MB (83.49%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        // 相当于图像中的坐标点和最低点
+        int sum = 0, minSum = 0;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            sum += gas[i] - cost[i];
+            if (sum < minSum) {
+                // 经过第 i 个站点后，使 sum 到达新低
+                // 所以站点 i + 1 就是最低点（起点）
+                start = i + 1;
+                minSum = sum;
+            }
+        }
+        if (sum < 0) {
+            // 总油量小于总的消耗，无解
+            return -1;
+        }
+        // 环形数组特性
+        if (start == n) {
+            return 0;
+        } else {
+            return start;
+        }
+            }
+        };
