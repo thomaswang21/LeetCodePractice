5897261a497bdcdfcd3aafbfbacbf285e3e08517
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-22 16:04:10 -0700

    Time: 7 ms (43.95%) | Memory: 10.9 MB (39.93%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int>hash;
-        int l=0, r=0;
+        unordered_map<char,int>window;
+        int left=0, right=0;
+        int windowMaxCount=0;
         int res=0;
-        int maxhash=0;
-        while(r<s.length()){
-            char c=s[r];
-            r++;
-            hash[c]++;
-            
-            maxhash=max(maxhash,hash[c]);
-            while(r-l-maxhash>k){
-                char d=s[l];
-                hash[d]--;
-                l++;
+        while(right<s.length()){
+            char c=s[right];
+            right++;
+            window[c]++;
+            windowMaxCount=max(windowMaxCount,window[c]);
+            while(right-left-windowMaxCount>k){
+                char d=s[left];
+                window[d]--;
+                left++;
             }
-            res=max(res, r-l);
+            res=max(res,right-left);
         }
         return res;
     }
