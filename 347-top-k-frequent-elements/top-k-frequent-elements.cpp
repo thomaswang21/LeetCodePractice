2ffd24eb809762cf4c5fb38bcb5f61889ca58620
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-22 14:32:30 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (83.02%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>hash;;
-        for(int i=0;i<nums.size();i++){
-            hash[nums[i]]++;
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>heap;
-        for(auto t:hash){
-            heap.push({t.second, t.first});
+    unordered_map<int,int>hash;
+    vector<int>ans;
+    for(int i=0;i<nums.size();i++){
+        hash[nums[i]]++;
+    }
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>heap;
+        for(auto t: hash){
+            heap.push({t.second,t.first});
             if(heap.size()>k)heap.pop();
         }
-        vector<int>ans;
         while(!heap.empty()){
-            ans.push_back(heap.top().second);
-            heap.pop();
+            
+        ans.push_back(heap.top().second);
+        heap.pop();
         }
-        return ans;
 
+    return ans;
     }
 };
