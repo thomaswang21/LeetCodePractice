d0b63516d435b22c3f173d5fb2181aa20365f6b8
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-21 18:39:08 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (87.97%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,int>> q;
+        int fresh = 0;
+        int time = 0;
+
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++) {
+                if(grid[i][j] == 1) fresh++;
+                if(grid[i][j] == 2) q.push({i, j});
+            }
+        }
+
+        vector<pair<int,int>> direction = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.empty() && fresh > 0) {
+            int length = q.size();
+            for(int i = 0; i < length; i++) {
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop(); // ✅ 别忘了 pop
+
+                for(int k = 0; k < 4; k++) {
+                    int row = r + direction[k].first;
+                    int col = c + direction[k].second;
+                    if(row >= 0 && row < grid.size() && col >= 0 && col < grid[0].size() && grid[row][col] == 1) {
+                        grid[row][col] = 2;
+                        q.push({row, col});
+                        fresh--;
+                    }
+                }
+            }
+            time++; // ✅ 每分钟腐烂一层，才 +1
+        }
+
+                    if (fresh == 0) {
+                    return time;
+                } else {
+                    return -1;
+                }
+
+                    }
+    };
