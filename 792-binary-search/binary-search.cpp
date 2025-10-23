bb8ec254aa74309b21b0d20aff7854a8958c9836
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-22 18:09:15 -0700

    Time: 0 ms (100.00%) | Memory: 31.2 MB (80.15%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left=0, right=nums.size()-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
+        int l=0, r=nums.size()-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
             if(nums[mid]==target){
                 return mid;
             }else if(nums[mid]>target){
-                right=mid-1;
+                r=mid-1;
             }else if(nums[mid]<target){
-                left=mid+1;
+                l=mid+1;
             }
         }
         return -1;
