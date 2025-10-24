4316552531b9bf759b773b45f8e8e9b165939c5d
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-23 20:47:17 -0700

    Time: 0 ms (100.00%) | Memory: 10.9 MB (29.46%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,41 +1,43 @@
-#include <vector>
-#include <list>
-using namespace std;
-
 class Solution {
-    vector<vector<int>> res; // 存放结果
-
 public:
-    // 主函数：输入一组不重复的数字，返回它们的全排列
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> track; // 记录「路径」
-        vector<bool> used(nums.size(), false); // 记录哪些元素已经使用过
+    // 记录所有全排列
+    vector<vector<int>> res;
+    // 记录当前正在穷举的排列
+    vector<int> track;
 
-        backtrack(nums, track, used);
+    // track 中的元素会被标记为 true，避免重复使用
+    vector<bool> used;
+
+    // 主函数，输入一组不重复的数字，返回它们的全排列
+    vector<vector<int>> permute(vector<int>& nums) {
+        used = vector<bool>(nums.size(), false);
+        
+        backtrack(nums);
         return res;
     }
 
-private:
-    // 路径：track
-    // 选择列表：nums 中未被使用的数字
-    // 结束条件：track 的长度等于 nums 的长度
-    void backtrack(const vector<int>& nums, vector<int>& track, vector<bool>& used) {
+    // 回溯算法核心框架，遍历回溯树，收集所有叶子节点上的全排列
+    void backtrack(vector<int>& nums) {
+        // 到达叶子节点，track 中的元素就是一个全排列
         if (track.size() == nums.size()) {
-            res.push_back(track); // 找到一个全排列
+            res.push_back(track);
             return;
         }
-
+        
         for (int i = 0; i < nums.size(); i++) {
-            if (used[i]) continue; // 跳过已使用的数字
-
+            // 排除不合法的选择
+            if (used[i]) {
+                // nums[i] 已经在 track 中，跳过
+                continue;
+            }
             // 做选择
             track.push_back(nums[i]);
             used[i] = true;
 
-            // 进入下一层决策树
-            backtrack(nums, track, used);
-
-            // 撤销选择
+            // 进入递归树的下一层
+            backtrack(nums);
+            
+            // 取消选择
             track.pop_back();
             used[i] = false;
         }
