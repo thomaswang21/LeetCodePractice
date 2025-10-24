faf2ab1d9fa89fc750feade4373175b015cd4f75
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-24 13:22:16 -0700

    Time: 0 ms (100.00%) | Memory: 10.6 MB (43.63%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,27 +1,19 @@
 class Solution {
-private:
-    vector<vector<int>> res;
-    vector<int> track;
-
 public:
+    vector<int>track;
+    vector<vector<int>>res;
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());  // 排序方便去重
-        backtrack(nums, 0);
+        sort(nums.begin(),nums.end());
+        backtrack(nums,0);
         return res;
     }
-
-private:
-    void backtrack(vector<int>& nums, int start) {
-        // 前序位置：每个节点的 track 都是一个子集
+    void backtrack(vector<int>&nums, int start){
         res.push_back(track);
-
-        for (int i = start; i < nums.size(); i++) {
-            // 去重：同一层递归跳过重复元素
-            if (i > start && nums[i] == nums[i - 1]) continue;
-
-            track.push_back(nums[i]);        // 选择当前元素
-            backtrack(nums, i + 1);          // 下一层从 i+1 开始
-            track.pop_back();                // 撤销选择
+        for(int i=start;i<nums.size();i++){
+            if(i>start&&nums[i]==nums[i-1])continue;
+            track.push_back(nums[i]);
+            backtrack(nums, i+1);
+            track.pop_back();
         }
     }
 };
