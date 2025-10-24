98ca31315323b0d4ac4cd29ce902e17b1cd681e9
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-23 18:55:29 -0700

    Time: 0 ms (100.00%) | Memory: 12.7 MB (77.89%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -14,6 +14,6 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p&&!q)return true;
         if(!p||!q||p->val!=q->val)return false;
-        return(isSameTree(p->left,q->left)&&isSameTree(p->right,q->right));
+        return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
     }
 };
