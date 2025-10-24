60c5161b4955ffb47eb8686112ae40f0cc829813
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-24 01:39:14 -0700

    Time: 0 ms (100.00%) | Memory: 8.1 MB (54.82%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>f(n+100);
+        f[0]=1;
+        f[1]=1;
+        for(int i=2; i<=n;i++){
+            f[i]=f[i-1]+f[i-2];
+        }
+        return f[n];
+    }
+};
