ecef5623c1b33744a7c4a12fba91119798e8a2f3
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-25 01:06:23 -0700

    Time: 36 ms (28.84%) | Memory: 27.2 MB (42.33%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.cpp
+++ 271-encode-and-decode-strings/encode-and-decode-strings.cpp
@@ -4,33 +4,31 @@ public:
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
         string res;
-        for(auto &s:strs){
-            res+=to_string(s.size())+"#"+s;
+        for (auto &s : strs) {
+            string len_str = to_string(s.size());
+            // 补齐 4 位，不够前面补 0
+            while (len_str.size() < 4) len_str = '0' + len_str;
+            res += len_str + s;
         }
         return res;
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
-        vector<string> res;
-        int i=0;
-        while(i<s.size()){
-            int j=i;
-            while(s[j]!='#')j++;
-            int length=stoi(s.substr(i, j-i));
-            i=j+1;
-            res.push_back(s.substr(i, length));
-            i+=length;
-
-            
+        vector<string> ans;
+        int i = 0;
+        while (i < s.size()) {
+            // 取 4 位长度字段
+            int length = stoi(s.substr(i, 4));
+            i += 4;
+            // 取对应长度的字符串
+            ans.emplace_back(s.begin() + i, s.begin() + i + length);
+            i += length;
         }
-        return res;
+        return ans;
     }
 };
 
-// Your Codec object will be instantiated and called as such:
-// Codec codec;
-// codec.decode(codec.encode(strs));
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
