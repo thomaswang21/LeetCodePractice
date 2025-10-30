e0b1848ed560c4aa3ed484c64501c987646b0fbc
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-10-29 23:21:42 -0700

    Time: 3 ms (60.35%) | Memory: 65 MB (83.56%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>incoming(n+1), outgoing(n+1);
+        for(auto &t:trust){
+            incoming[t[1]]++;
+            outgoing[t[0]]++;
+        }
+        for(int i=1; i<=n;i++){
+            if(incoming[i]==n-1&&outgoing[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
