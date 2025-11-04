ef7e122fb91d621732ac3d047cbf17bc3db0a569
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-03 16:31:43 -0800

    Time: 3 ms (73.09%) | Memory: 18.2 MB (75.03%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> g(n);
-        vector<int> d(n);
-        for (auto& e: edges) {
-            int b = e[0], a = e[1];
-            g[a].push_back(b);
-            d[b] ++ ;
-        }
-        queue<int> q;
-        for (int i = 0; i < n; i ++ )
-            if (d[i] == 0)
-                q.push(i);
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses); // 邻接表
+        vector<int> indegree(numCourses);      // 入度数组
 
-        vector<int> res;
-        while (q.size()) {
-            auto t = q.front();
+        // 建图: 先学b再学a，所以边是 b -> a
+        for (auto& pre : prerequisites) {
+            int a = pre[0], b = pre[1];
+            graph[b].push_back(a); // b -> a
+            indegree[a]++;         // a的入度加一
+        }
+
+        queue<int> q;
+        // 把所有入度为0的课入队
+        for (int i = 0; i < numCourses; ++i) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> order; // 记录学习顺序
+
+        while (!q.empty()) {
+            int course = q.front();
             q.pop();
-            res.push_back(t);
-            for (int i: g[t]){
-               d[i] = d[i] - 1;  // 入度减 1
-                if (d[i] == 0){
-                    q.push(i);
+            order.push_back(course); // 加入学习顺序
+
+            for (int next : graph[course]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.push(next);
                 }
             }
         }
-        if (res.size() < n) res = {};
-        return res;
+
+        // 如果能学完所有课程，则返回顺序，否则返回空数组
+        if (order.size() == numCourses) {
+            return order;
+        } else {
+            return {};
+        }
     }
 };
