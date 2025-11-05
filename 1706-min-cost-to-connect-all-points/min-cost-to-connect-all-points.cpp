ed50da98ed221f4a194e2867b57dc2a90d257905
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-04 20:28:02 -0800

    Time: 27 ms (89.96%) | Memory: 14.6 MB (89.55%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<int> minDist(n, INT_MAX);
+        vector<bool> inMST(n, false);
+        minDist[0] = 0;
+        int result = 0;
+
+        for (int i = 0; i < n; ++i) {
+            int u = -1;
+            for (int j = 0; j < n; ++j) {
+                if (!inMST[j] && (u == -1 || minDist[j] < minDist[u])) {
+                    u = j;
+                }
+            }
+
+            inMST[u] = true;
+            result += minDist[u];
+
+            for (int v = 0; v < n; ++v) {
+                if (!inMST[v]) {
+                    int dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                    if (dist < minDist[v]) {
+                        minDist[v] = dist;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+};
