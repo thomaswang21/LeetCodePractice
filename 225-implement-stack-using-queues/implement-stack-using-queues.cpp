7bac575a2ccaebdd3ec60f369eb6849347fee028
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-10 03:25:23 -0800

    Time: 0 ms (100.00%) | Memory: 9.5 MB (19.00%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -46,3 +46,12 @@ public:
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
