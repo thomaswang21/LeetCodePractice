c2f6521226add00d0e6c2ce849c8d439737fc58c
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-12 19:15:30 -0800

    Time: 23 ms (91.46%) | Memory: 68.4 MB (66.21%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {
-        priority_queue<pair<int,int>>pg;
-        for(int i=0; i<p.size();i++){
-            int dist=p[i][0]*p[i][0]+p[i][1]*p[i][1];
-            pg.push({-1*dist, i});
+        priority_queue<pair<int,int>> pg;
+        for (int i = 0; i < p.size(); i++) {
+            int dist = p[i][0] * p[i][0] + p[i][1] * p[i][1];
+            pg.push({-dist, i});
         }
-        vector<vector<int>>ans;
-        while(k--){
-            auto x=pg.top();
+
+        vector<vector<int>> ans;
+        for (int i = 0; i < k; i++) {  // 用 for 循环替代 while
+            auto x = pg.top();
             pg.pop();
             ans.push_back(p[x.second]);
         }
+
         return ans;
     }
 };
