52fd0e993df17c1b9d67a201e2de21897be48000
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-16 11:16:02 -0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,32 +1,28 @@
-
-// 思路：
-// 这是 LeetCode 第 70 题 Climbing Stairs（爬楼梯）
-// 每次可以爬 1 或 2 阶台阶，问到达第 n 阶有多少种不同方法。
-// 这是经典的动态规划问题，等价于斐波那契数列。
-// 状态转移方程：f(n) = f(n - 1) + f(n - 2)
-// 初始条件：f(1) = 1, f(2) = 2
-
-#include <bits/stdc++.h>
-using namespace std;
+#include <vector>
 
 class Solution {
 public:
+    // 备忘录
+    std::vector<int> memo;
+
     int climbStairs(int n) {
-        // 若台阶数小于等于 2，直接返回 n
-        if (n <= 2) return n;
+        memo.resize(n + 1, 0);
+        return dp(n);
+    }
 
-        int a = 1; // f(1)
-        int b = 2; // f(2)
-        int c = 0; // 临时变量存放 f(i)
-
-        // 从第 3 阶开始迭代
-        for (int i = 3; i <= n; i++) {
-            c = a + b; // f(i) = f(i-1) + f(i-2)
-            a = b;     // 更新 f(i-2)
-            b = c;     // 更新 f(i-1)
+    // 定义：爬到第 n 级台阶的方法个数为 dp(n)
+    int dp(int n) {
+        // base case
+        if (n <= 2) {
+            return n;
         }
-
-        return b; // 返回 f(n)
+        if (memo[n] > 0) {
+            return memo[n];
+        }
+        // 状态转移方程：
+        // 爬到第 n 级台阶的方法个数等于爬到 n - 1 的方法个数和爬到 n - 2 的方法个数之和。
+        memo[n] = dp(n - 1) + dp(n - 2);
+        return memo[n];
     }
 };
 
