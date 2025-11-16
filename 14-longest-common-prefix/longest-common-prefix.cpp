28b7feea39c718de2a746c5ab06d081bb9963db2
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-16 10:47:12 -0800

    Time: 0 ms (100.00%) | Memory: 11.8 MB (68.62%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int m = strs.size();
-        // 以第一行的列数为基准
-        int n = strs[0].length();
-        for (int col = 0; col < n; col++) {
-            for (int row = 1; row < m; row++) {
-                string thisStr = strs[row], prevStr = strs[row - 1];
-                // 判断每个字符串的 col 索引是否都相同
-                if (col >= thisStr.length() || col >= prevStr.length() ||
-                    thisStr[col] != prevStr[col]) {
-                    // 发现不匹配的字符，只有 strs[row][0..col-1] 是公共前缀
-                    return strs[row].substr(0, col);
-                }
+        if(strs.empty())return"";
+        string prefix=strs[0];
+        for(int i=1;i<strs.size();i++){
+            int j=0;
+            while(j<prefix.size()&&j<strs[i].size()&&prefix[j]==strs[i][j]){
+                j++;
+            }
+            prefix=prefix.substr(0,j);
+            if(prefix.empty()){
+                return"";
             }
         }
-        return strs[0];
+        return prefix;
     }
 };
