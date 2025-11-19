dc4b2b10166d0d4f01926cfc4376636cc71a7fae
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-18 23:39:11 -0800

    Time: 59 ms (20.60%) | Memory: 153.3 MB (12.92%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        priority_queue<pair<int,int>>heap;
-        vector<int>output;
-        for(int i=0;i<nums.size();i++){
-            heap.push({nums[i],i});
-            if(i>=k-1){
-                while(heap.top().second<=i-k){
-                    heap.pop();
+        priority_queue<pair<int,int>> pq;   // 大顶堆：{值, 下标}
+        vector<int> res;
+        int left = 0;  // left 是窗口左端
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            // -------- 扩大窗口：加入 right 元素 --------
+            pq.push({nums[right], right});
+
+            // -------- 如果窗口超过 k，缩小窗口 --------
+            while (right - left + 1 > k) {
+                left++; // 移动左指针
+            }
+
+            // -------- 当窗口大小刚好 = k，处理答案 --------
+            if (right - left + 1 == k) {
+
+                // 堆顶可能是过期元素（在窗口左边界之前），需要把它弹掉
+                while (!pq.empty() && pq.top().second < left) {
+                    pq.pop();
                 }
-                output.push_back(heap.top().first);
+
+                // 此时堆顶就是窗口最大值
+                res.push_back(pq.top().first);
             }
         }
-        return output;
+
+        return res;
     }
 };
