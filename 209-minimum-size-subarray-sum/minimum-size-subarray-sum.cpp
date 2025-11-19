e04a9d1c25dd1c03293a4487020527da47dbe92e
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-19 12:59:44 -0800

    Time: 3 ms (12.87%) | Memory: 32 MB (71.43%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int left = 0, right = 0;
-        // 维护窗口内元素之和
-        int windowSum = 0;
-        int res = INT_MAX;
-
-        while (right < nums.size()) {
-            // 扩大窗口
-            windowSum += nums[right];
+        int left,right=0;
+        int res=INT_MAX;
+        int windowSum=0;
+        while(right<nums.size()){
+            windowSum+=nums[right];
             right++;
-            while (windowSum >= target && left < right) {
-                // 已经达到 target，缩小窗口，同时更新答案
-                res = min(res, right - left);
-                windowSum -= nums[left];
-                left++;
+            while(windowSum>=target&&left<right){
+                    res=min(res, right-left);
+                    windowSum-=nums[left];
+                    left++;
             }
         }
-        return res == INT_MAX ? 0 : res;
+        if (res == INT_MAX) {
+                return 0;
+        } else {
+                return res;
+}
+
     }
 };
