bfea04f41d9e6134d9a849191d8ede7611dbae84
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-19 13:47:19 -0800

    Time: 43 ms (85.16%) | Memory: 29.1 MB (47.83%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,7 +4,6 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>>res;
         for(int i=0; i<nums.size();i++){
-            if(nums[i]>0)break;
             if(i>0&&nums[i]==nums[i-1])continue;
             int l=i+1, r=nums.size()-1;
             while(l<r){
