c62d1056c85984d2305bef11ba63a82a00743c2f
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-19 12:45:04 -0800

    Time: 40 ms (24.12%) | Memory: 139.2 MB (59.29%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,33 +1,20 @@
-#include <vector>
-#include <deque>
-using namespace std;
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> res;
-        deque<int> dq; // 存储索引，保证 nums[dq.front()] 是当前窗口最大值
-
-        for (int i = 0; i < nums.size(); ++i) {
-            // -------- 移除窗口外的元素 --------
-            while (!dq.empty() && dq.front() <= i - k) {
+        vector<int>res;
+        deque<int>dq;
+        for(int i=0;i<nums.size();i++){
+            while(!dq.empty()&&dq.front()<=i-k){
                 dq.pop_front();
             }
-
-            // -------- 移除比当前元素小的元素（保持递减） --------
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+            while(!dq.empty()&&nums[dq.back()]<=nums[i]){
                 dq.pop_back();
             }
-
-            // -------- 将当前元素加入队列 --------
             dq.push_back(i);
-
-            // -------- 当窗口大小 >= k，记录答案 --------
-            if (i >= k - 1) {
+            if(i>=k-1){
                 res.push_back(nums[dq.front()]);
             }
         }
-
         return res;
     }
 };
