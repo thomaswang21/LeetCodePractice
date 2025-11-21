3c455659af2f96aec239c53a774c98804da99d91
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-20 18:33:04 -0800

    Time: 0 ms (100.00%) | Memory: 31.4 MB (8.79%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -6,10 +6,10 @@ public:
             int mid=l+(r-l)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]>target){
-                r=mid-1;
             }else if(nums[mid]<target){
                 l=mid+1;
+            }else{
+                r=mid-1;
             }
         }
         return -1;
