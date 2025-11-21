f3f6fcab60793013f6aba9bba3ae5aee734ee1d8
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-20 16:28:44 -0800

    Time: 19 ms (72.90%) | Memory: 107.2 MB (55.60%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& T) {
-        stack<int> st;
-        int n=T.size();
-        vector<int> res(n);
-        for(int i=0; i<n; i++)
-        {
-            while(!st.empty() && T[st.top()]<T[i])
-            {
-                res[st.top()]=i-st.top();
-                st.pop();
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> res(n, 0);
+        stack<int> s; // 存索引
+
+        for (int i = 0; i < n; i++) {
+            // 当前温度大于栈顶索引对应的温度
+            while (!s.empty() && temperatures[i] > temperatures[s.top()]) {
+                int idx = s.top();
+                s.pop();
+                res[idx] = i - idx; // 更新栈顶元素的等待天数
             }
-            st.push(i);
+            s.push(i); // 当前元素入栈
         }
+
         return res;
     }
 };
+
 
