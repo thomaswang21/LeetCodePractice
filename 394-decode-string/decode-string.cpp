b3ec121d78f1d58d7809571d21aa94b259ccde98
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-20 17:42:00 -0800

    Time: 0 ms (100.00%) | Memory: 8.4 MB (95.12%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -1,30 +1,33 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
 class Solution {
 public:
     string decodeString(string s) {
-        vector<string> stringStack;
-        vector<int> countStack;
+        stack<string> stringStack; // 存上一级字符串
+        stack<int> countStack;     // 存对应的重复次数
         string cur = "";
         int k = 0;
 
         for (char c : s) {
             if (isdigit(c)) {
-                k = k * 10 + (c - '0');
+                k = k * 10 + (c - '0'); // 解析数字
             } else if (c == '[') {
-                stringStack.push_back(cur);
-                countStack.push_back(k);
-                cur = "";
+                stringStack.push(cur);  // 保存当前字符串
+                countStack.push(k);    // 保存重复次数
+                cur = "";               // 开始新的子串
                 k = 0;
             } else if (c == ']') {
-                string temp = cur;
-                cur = stringStack.back();
-                stringStack.pop_back();
-                int count = countStack.back();
-                countStack.pop_back();
+                string temp = cur;      // 当前要重复的子串
+                cur = stringStack.top(); stringStack.pop();
+                int count = countStack.top(); countStack.pop();
                 for (int i = 0; i < count; i++) {
                     cur += temp;
                 }
             } else {
-                cur += c;
+                cur += c;               // 普通字符加入当前字符串
             }
         }
 
