f64e8c8eac75f41156e590e9217c51cb48f7f442
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-23 14:24:45 -0800

    Time: 8 ms (59.48%) | Memory: 23 MB (13.07%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -4,17 +4,18 @@ public:
         int left=1, right=1e9+1;
         while(left<right){
             int mid=left+(right-left)/2;
-            if(f(piles, mid)<=h){
+            if(f(piles,mid)<=h){
                 right=mid;
             }else{
                 left=mid+1;
             }
+            
         }
         return left;
     }
     long f(vector<int>&piles, int x){
         long hours=0;
-        for(int i=0;i<piles.size();i++){
+        for(int i=0; i<piles.size();i++){
             hours+=piles[i]/x;
             if(piles[i]%x>0){
                 hours++;
