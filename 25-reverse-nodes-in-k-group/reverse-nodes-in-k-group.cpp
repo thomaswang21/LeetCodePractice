3009bfef77477f7a45245f0a4efc9b64e2d6ba43
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-23 10:30:21 -0800

    Time: 0 ms (100.00%) | Memory: 16.6 MB (37.46%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-   ListNode* reverseKGroup(ListNode* head, int k) {
-    ListNode* dummy = new ListNode(-1);
-    dummy->next = head;
-    ListNode* a = dummy;   // 原 prev
-    ListNode* b = head;    // 原 curr
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1) return head;
 
-    // 计算链表长度
-    int length = 0;
-    ListNode* temp = head;
-    while (temp != nullptr) {
-        length++;
-        temp = temp->next;
-    }
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* prevGroupEnd = dummy; // 上一组的尾节点
 
-    // 分组反转
-    for (int i = 0; i < length / k; i++) {
-        for (int j = 1; j < k; j++) {
-            ListNode* c = b->next;   // 原 next
-            b->next = c->next;
-            c->next = a->next;
-            a->next = c;
+        while (true) {
+            // 找到这一组的末尾节点
+            ListNode* groupStart = prevGroupEnd->next;
+            ListNode* groupEnd = prevGroupEnd;
+            for (int i = 0; i < k && groupEnd; i++) groupEnd = groupEnd->next;
+            if (!groupEnd) break; // 不足 k 个节点，不反转
+
+            ListNode* nextGroupStart = groupEnd->next;
+
+            // 反转当前组
+            ListNode* prev = nextGroupStart;
+            ListNode* curr = groupStart;
+            while (curr != nextGroupStart) {
+                ListNode* tmp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = tmp;
+            }
+
+            // 连接上一组和当前组
+            prevGroupEnd->next = groupEnd;
+            prevGroupEnd = groupStart; // 现在 groupStart 是组尾
         }
-        a = b;
-        b = a->next;
-    }
 
-    return dummy->next;
-}
+        return dummy->next;
+    }
 };
+
