d1f1b6b955c9fbe52fb0fb1a14b31faa386613aa
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-24 12:22:06 -0800

    Time: 0 ms (100.00%) | Memory: 22.2 MB (9.20%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,32 +1,42 @@
-
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-vector<int> vc;
-void inorder(TreeNode *root) {
-    if (root == nullptr) return;
-    inorder(root -> left);
-    vc.emplace_back(root -> val);
-    inorder(root -> right);
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+vector<int> vc;
+void inorder(TreeNode *root) {
+    if (root == nullptr) return;
+    inorder(root -> left);
+    vc.emplace_back(root -> val);
+    inorder(root -> right);
 }
-class Solution {
+class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
-        vc.clear();
-        inorder(root);
-        for (int i = 0; i < (int)vc.size() - 1; i++) {
-            if (vc[i] >= vc[i + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
+    bool isValidBST(TreeNode* root) {
+        vc.clear();
+        inorder(root);
+        for (int i = 0; i < (int)vc.size() - 1; i++) {
+            if (vc[i] >= vc[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 };
