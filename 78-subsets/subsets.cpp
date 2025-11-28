1ecfffd39db06f33d7c02921990f5653d7dba1f3
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-28 11:09:08 -0800

    Time: 0 ms (100.00%) | Memory: 10.1 MB (35.30%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>res;
+    vector<vector<int>>ans;
     vector<int>track;
+    int n;
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        backtrack(nums,0);
-        return res;
+        dfs(nums, 0);
+        n=nums.size();
+        return ans;
     }
-    void backtrack(vector<int>&nums,int start){
-        res.push_back(track);
-        for(int i=start;i<nums.size();i++){
+    void dfs(vector<int>&nums, int start){
+        n=nums.size();
+        ans.push_back(track);
+        for(int i=start;i<n;i++){
             track.push_back(nums[i]);
-            backtrack(nums, i+1);
+            dfs(nums, i+1);
             track.pop_back();
         }
     }
