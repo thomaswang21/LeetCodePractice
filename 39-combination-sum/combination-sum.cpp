958cf6f5de0ea7ce119c4a6be372886a1bf58f74
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-27 18:49:37 -0800

    Time: 3 ms (53.99%) | Memory: 14.4 MB (39.84%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    vector<int>track;
-    vector<vector<int>>ans;
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        backtrack(nums, 0,target,0);
+    vector<int> track;
+    vector<vector<int>> ans;
+
+    vector<vector<int>> combinationSum(vector<int> &nums, int target) {
+        backtrack(nums, 0, target, 0);
         return ans;
     }
-    void backtrack(vector<int>&nums,int start, int target, int sum){
-        if(sum==target){
+
+    void backtrack(vector<int> &nums, int start, int target, int sum) {
+        if (sum == target) {
             ans.push_back(track);
             return;
         }
-        if(sum>target){
-            return;
-        }
-        for(int i=start;i<nums.size();i++){
+        if (sum > target) return;
+
+        for (int i = start; i < nums.size(); i++) {
             track.push_back(nums[i]);
-            sum+=nums[i];
-            backtrack(nums, i, target,sum);
-            sum-=nums[i];
+            sum += nums[i];
+
+            backtrack(nums, i, target, sum);  // ⭐ 允许重复选同一个 i
+
+            sum -= nums[i];
             track.pop_back();
-            
         }
     }
 };
