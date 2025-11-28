962f428d86aa5c736ba504e0f4dcd1c940c809c5
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-28 11:14:38 -0800

    Time: 3 ms (34.59%) | Memory: 10.7 MB (43.13%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int>track;
-    vector<vector<int>>res;
+    vector<vector<int>>ans;
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        backtrack(nums,0);
-        return res;
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0);
+        
+        return ans;
     }
-    void backtrack(vector<int>&nums, int start){
-        res.push_back(track);
+    void dfs(vector<int>&nums, int start){
+        ans.push_back(track);
         for(int i=start;i<nums.size();i++){
             if(i>start&&nums[i]==nums[i-1])continue;
             track.push_back(nums[i]);
-            backtrack(nums, i+1);
+            dfs(nums, i+1);
             track.pop_back();
         }
     }
