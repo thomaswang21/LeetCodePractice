ec16c2661da23f9b5ad81b1ee886c283991bd8c1
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-27 18:31:19 -0800

    Time: 0 ms (100.00%) | Memory: 11 MB (29.31%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -4,26 +4,21 @@ public:
     vector<vector<int>> res;
     // 记录当前正在穷举的排列
     vector<int> track;
-
     // track 中的元素会被标记为 true，避免重复使用
     vector<bool> used;
-
     // 主函数，输入一组不重复的数字，返回它们的全排列
     vector<vector<int>> permute(vector<int>& nums) {
-        used = vector<bool>(nums.size(), false);
-        
+        used = vector<bool>(nums.size(), false);        
         backtrack(nums);
         return res;
     }
-
     // 回溯算法核心框架，遍历回溯树，收集所有叶子节点上的全排列
     void backtrack(vector<int>& nums) {
         // 到达叶子节点，track 中的元素就是一个全排列
         if (track.size() == nums.size()) {
             res.push_back(track);
             return;
-        }
-        
+        }       
         for (int i = 0; i < nums.size(); i++) {
             // 排除不合法的选择
             if (used[i]) {
@@ -33,10 +28,8 @@ public:
             // 做选择
             track.push_back(nums[i]);
             used[i] = true;
-
             // 进入递归树的下一层
-            backtrack(nums);
-            
+            backtrack(nums);            
             // 取消选择
             track.pop_back();
             used[i] = false;
