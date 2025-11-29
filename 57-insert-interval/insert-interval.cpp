648356d36665e148c14d49fcbb604916e2f135fa
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-29 12:38:28 -0800

    Time: 0 ms (100.00%) | Memory: 21.8 MB (45.36%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& b) {
-        vector<vector<int>> res;
-        int k = 0;
-        
-        // 处理左边无交集的部分
-        while (k < a.size() && a[k][1] < b[0]) {
+        vector<vector<int>>res;
+        int k=0;
+        while(k<a.size()&&a[k][1]<b[0]){
             res.push_back(a[k]);
             k++;
         }
-
-        // 处理重叠部分，合并区间
-        if (k < a.size()) {
-            b[0] = min(b[0], a[k][0]);
-            while (k < a.size() && a[k][0] <= b[1]) {
-                b[1] = max(b[1], a[k][1]);
+        if(k<a.size()){
+            b[0]=min(b[0],a[k][0]);
+            while(k<a.size()&&a[k][0]<=b[1]){
+                b[1]=max(b[1],a[k][1]);
                 k++;
             }
+           
         }
-        res.push_back(b);
-
-        // 处理右边无交集的部分
-        while (k < a.size()) {
+         res.push_back(b);
+        while(k<a.size()){
             res.push_back(a[k]);
             k++;
         }
-
         return res;
+
     }
 };
