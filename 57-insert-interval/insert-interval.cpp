901dbb35c1e43e5518e8274d70842ace3e6b243d
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-29 12:46:07 -0800

    Time: 0 ms (100.00%) | Memory: 21.7 MB (45.36%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -8,14 +8,14 @@ public:
             k++;
         }
         if(k<a.size()){
-            b[0]=min(b[0],a[k][0]);
+            b[0]=min(b[0], a[k][0]);
             while(k<a.size()&&a[k][0]<=b[1]){
-                b[1]=max(b[1],a[k][1]);
+                b[1]=max(a[k][1],b[1]);
                 k++;
             }
-           
+
         }
-         res.push_back(b);
+        res.push_back(b);
         while(k<a.size()){
             res.push_back(a[k]);
             k++;
