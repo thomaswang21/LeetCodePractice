a55e56352b8644e8bbbbb9bd8f965d7ee85c3678
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-11-30 15:56:12 -0800

    Time: 0 ms (100.00%) | Memory: 9.1 MB (69.26%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> f(n, vector<int>(m));
+        for (int i = 0; i < n; i ++ )
+            for (int j = 0; j < m; j ++ )
+                if (!i && !j) f[i][j] = 1;
+                else {
+                    if (i) f[i][j] += f[i - 1][j];
+                    if (j) f[i][j] += f[i][j - 1];
+                }
+
+        return f[n - 1][m - 1];
+    }
+};
+
