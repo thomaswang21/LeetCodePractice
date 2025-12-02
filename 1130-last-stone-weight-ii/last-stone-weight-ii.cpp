0a327134a7cf7d573feb11a5861bffe8cec472eb
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-01 21:12:20 -0800

    Time: 1 ms (61.82%) | Memory: 11.5 MB (53.72%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        int sum = 0;
-        for (int stone : stones) {
-            sum += stone;
+    int sum = 0;
+    for (int x : stones) sum += x;
+
+    int target = sum / 2;
+    vector<int> dp(target + 1, 0);
+
+    for (int w : stones) {
+        for (int j = target; j >= w; j--) {
+            dp[j] = max(dp[j], dp[j - w] + w);
         }
-        // 背包问题状态转移方程
-        // 定义：仅使用前 i 个元素，背包容量为 j 时，能够装的最大重量为 dp[i][j]
-        vector<vector<int>> dp(stones.size() + 1, vector<int>(sum / 2 + 1, 0));
-        for (int i = 1; i <= stones.size(); i++) {
-            // 当前石头重量，注意下标从 1 开始
-            int curStone = stones[i - 1];
-            for (int j = 0; j <= sum / 2; j++) {
-                // 对于第 i 个物品，有两种情况：
-                // 1. 不选第 i 个物品，则 dp[i][j] = dp[i - 1][j]
-                // 2. 选第 i 个物品，则 dp[i][j] = dp[i - 1][j - curStone] + curStone
-                if (j >= curStone) {
-                    // 可以装下第 i 个物品，则两种选择中取最大值
-                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - curStone] + curStone);
-                } else {
-                    // 装不下第 i 个物品，则只能选择不装
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        // 背包容量为 sum/2 时，能够装的最大重量
-        int maxWeight = dp[stones.size()][sum / 2];
-        // 两个子集的重量差
-        return sum -2* maxWeight;
     }
+
+    return sum - 2 * dp[target];
+}
+
 };
