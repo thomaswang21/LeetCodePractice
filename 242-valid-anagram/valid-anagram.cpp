4879e2e063b624efd0792b4e94b28c073b42e6ef
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-05 10:29:40 -0800

    Time: 2 ms (54.84%) | Memory: 9.8 MB (47.40%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,7 +7,7 @@ public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
 
-        unordered_map<char, int> count,read;
+        unordered_map<char, int> count;
 
         // 统计 s 中每个字符的频率
         for (char c : s) {
@@ -16,11 +16,11 @@ public:
 
         // 对 t 中的每个字符进行抵消
         for (char c : t) {
-            read[c]++;
-           // 若某字符数量变负，说明 t 含有多余字符
+            count[c]--;
+            if (count[c] < 0) return false; // 若某字符数量变负，说明 t 含有多余字符
         }
 
-        return count==read; // 所有字符都抵消完即为异位词
+        return true; // 所有字符都抵消完即为异位词
     }
 };
 
