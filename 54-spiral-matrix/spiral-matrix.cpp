9cf717c7671be12320448b27242fef3d5db8ced5
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-06 17:05:51 -0800

    Time: 0 ms (100.00%) | Memory: 9.5 MB (14.16%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int>res;
-        int n=matrix.size();
-        int m=matrix[0].size();
+        vector<int> res;
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        int dx[]={0,1,0,-1},dy[]={1,0,-1,0};
-        vector<vector<bool>>st(n, vector<bool>(m));
-        for(int i=0, x=0, y=0, d=0; i<n*m;i++){
+        vector<int> dx = {0, 1, 0, -1};
+        vector<int> dy = {1, 0, -1, 0};
+
+        vector<vector<bool>> st(n, vector<bool>(m, false));
+
+        for (int i = 0, x = 0, y = 0, d = 0; i < n * m; i++) {
             res.push_back(matrix[x][y]);
-            st[x][y]=true;
-
-            int a=x+dx[d], b=y+dy[d];
-            if(a<0||a>=n||b<0||b>=m||st[a][b]){
-                d=(d+1)%4;
-                a=x+dx[d], b=y+dy[d];
+            st[x][y] = true;
+            int a = x + dx[d], b = y + dy[d];
+            if (a < 0 || a >= n || b < 0 || b >= m || st[a][b]) {
+                d = (d + 1) % 4;
+                a = x + dx[d];
+                b = y + dy[d];
             }
-            x=a, y=b;
+            x = a;
+            y = b;
         }
         return res;
     }
