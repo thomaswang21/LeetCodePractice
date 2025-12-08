139188a9c112250837f725520e84ef6eb69b473c
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-08 02:03:29 -0800

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.70%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0, j = 0;
-        vector<int> ans;
-        
-        while (i < m || j < n) {
-            if (i < m && (j >= n || nums1[i] <= nums2[j])) {
-                ans.push_back(nums1[i]);
-                i++;
+        // Pointer for the last valid element in nums1
+        int p1 = m - 1;
+        // Pointer for the last element in nums2
+        int p2 = n - 1;
+        // Pointer for the last position in the merged array (nums1)
+        int p = m + n - 1;
+
+        // While both arrays have elements to compare
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
             } else {
-                ans.push_back(nums2[j]);
-                j++;
+                nums1[p] = nums2[p2];
+                p2--;
             }
+            p--;
         }
-        
-        nums1 = ans;
+
+        // If there are still elements left in nums2, copy them over.
+        // (If elements are left in nums1, they are already in the correct place)
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
     }
 };
