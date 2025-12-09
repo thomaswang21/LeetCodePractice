d0bcbedf6b2b4bb152b1aebd6b2c50d8b3a3156c
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-08 22:44:27 -0800

    Time: 0 ms (100.00%) | Memory: 23.5 MB (47.81%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -32,3 +32,12 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
