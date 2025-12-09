c9f5ff147c55aae10193702ffcd3467062697ee9
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-08 19:26:59 -0800

    Time: 5 ms (74.68%) | Memory: 55.7 MB (84.37%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -3,20 +3,21 @@ public:
     long long time;
     unordered_map<int, set<int>>following;
     unordered_map<int, vector<pair<int,int>>>tweets;
-
     Twitter() {
         time=0;
     }
     
     void postTweet(int userId, int tweetId) {
-        tweets[userId].push_back({time++, tweetId});
+        tweets[userId].push_back({time, tweetId});
+        time++;
+
     }
     
     vector<int> getNewsFeed(int userId) {
         vector<int>res;
         priority_queue<pair<int,int>>pq;
         for(auto it:tweets[userId])pq.push(it);
-        for(int follower: following[userId]){
+        for(int follower:following[userId]){
             for(auto it:tweets[follower])pq.push(it);
         }
         while(!pq.empty()&&res.size()<10){
@@ -34,24 +35,6 @@ public:
         following[followerId].erase(followeeId);
     }
 };
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
 
 /**
  * Your Twitter object will be instantiated and called as such:
