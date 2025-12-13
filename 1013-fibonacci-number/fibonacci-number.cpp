5a3f3de91739ce92cd1194cf140e64ee8d7e2c60
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-12 18:52:04 -0800

    Time: 3 ms (62.50%) | Memory: 8.4 MB (11.44%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,7 +6,7 @@ public:
         vector<int>F(n+1);
         F[0]=0;
         F[1]=1;
-        for(int i=2; i<n+1;i++){
+        for(int i=2; i<=n;i++){
             F[i]=F[i-1]+F[i-2];
         }
         return F[n];
