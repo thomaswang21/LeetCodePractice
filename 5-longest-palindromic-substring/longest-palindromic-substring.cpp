d83ce8b5a22a118350d388101ab4dd271d1c38aa
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-13 02:15:36 -0800

    Time: 7 ms (86.10%) | Memory: 14 MB (47.17%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -8,7 +8,7 @@ public:
             while(res.size()<r-l-1)res=s.substr(l+1,r-l-1);
             l=i, r=i+1;
             while(l>=0&&r<s.size()&&s[l]==s[r])l--,r++;
-            if (res.size() < r - l - 1) res = s.substr(l + 1, r - l - 1);
+          while (res.size() < r - l - 1) res = s.substr(l + 1, r - l - 1);
 
         }
         return res;
