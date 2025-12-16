5929e4f375dd0a26f841a3fc3ae0faf131100a17
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-15 17:52:19 -0800

    Time: 0 ms (100.00%) | Memory: 12.2 MB (25.77%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string, vector<pair<string, double>>> adj; // Map a -> list of [b, a/b]
-
-        for (int i = 0; i < equations.size(); i++) {
-            string a = equations[i][0];
-            string b = equations[i][1];
+        unordered_map<string, vector<pair<string, double>>>adj;
+        for(int i=0;i<equations.size();i++){
+            string a=equations[i][0];
+            string b=equations[i][1];
             adj[a].emplace_back(b, values[i]);
-            adj[b].emplace_back(a, 1.0 / values[i]);
+            adj[b].emplace_back(a, 1.0/values[i]);
         }
-        vector<double> res;
-        for (auto& query : queries) {
-            string src = query[0];
-            string target = query[1];
+        vector<double>res;
+        for(auto query: queries){
+            string src=query[0];
+            string target=query[1];
             res.push_back(bfs(src, target, adj));
         }
-
         return res;
     }
-private:
-    double bfs(string& src, const string& target, unordered_map<string, vector<pair<string, double>>>& adj) {
-        if (!adj.count(src) || !adj.count(target)) {
-            return -1.0;
-        }
-
-        queue<pair<string, double>> q;
-        unordered_set<string> visited;
-        q.emplace(src, 1.0);
+    double bfs(string &src, string target, unordered_map<string, vector<pair<string, double>>> &adj ){
+        if(!adj.count(src)||!adj.count(target))return -1.0;
+        queue<pair<string, double>>q;
+        unordered_set<string>visited;
+        q.emplace(src,1);
         visited.insert(src);
-        
-        while (!q.empty()) {
-            auto [node, weight] = q.front();
+        while(!q.empty()){
+            auto[node, weight]=q.front();
             q.pop();
-
-            if (node == target) {
+            if(node==target){
                 return weight;
             }
-
-            for (const auto& [nei, neiWeight] : adj[node]) {
-                if (!visited.count(nei)) {
+            for(auto &[nei, neiweight]:adj[node]){
+                if(!visited.count(nei)){
                     visited.insert(nei);
-                    q.emplace(nei, weight * neiWeight);
+                    q.emplace(nei,weight * neiweight);
                 }
             }
         }
