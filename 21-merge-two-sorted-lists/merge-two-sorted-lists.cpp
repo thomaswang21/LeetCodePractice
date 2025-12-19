4ddeed85f35106e361c78d4f20886237b63ad2e0
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-18 16:49:47 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (9.13%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -16,13 +16,12 @@ public:
         while(l1&&l2){
             if(l1->val<l2->val){
                 p->next=l1;
-                l1=l1->next;
                 p=p->next;
-                
+                l1=l1->next;
             }else{
                 p->next=l2;
-                p=p->next;
                 l2=l2->next;
+                p=p->next;
             }
         }
         if(l1)p->next=l1;
