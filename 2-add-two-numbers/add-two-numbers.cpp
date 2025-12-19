444bd744ee4a8db14ed785b2f36111ce51141e0d
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2025-12-18 22:20:51 -0800

    Time: 0 ms (100.00%) | Memory: 77.1 MB (43.89%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -17,7 +17,8 @@ public:
         while(l1||l2||t){
             if(l1)t+=l1->val, l1=l1->next;
             if(l2)t+=l2->val, l2=l2->next;
-            p=p->next=new ListNode(t%10);
+            p->next=new ListNode(t%10);
+            p=p->next;
             t/=10;
         }
         return dummy->next;
