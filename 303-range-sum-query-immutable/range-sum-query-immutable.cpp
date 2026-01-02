d8b60f5671ce20bd879a1bd1782bb10e748b137a
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-02 13:00:15 -0800

    Time: 1 ms (57.25%) | Memory: 23.9 MB (59.37%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -18,3 +18,9 @@ public:
  * NumArray* obj = new NumArray(nums);
  * int param_1 = obj->sumRange(left,right);
  */
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
