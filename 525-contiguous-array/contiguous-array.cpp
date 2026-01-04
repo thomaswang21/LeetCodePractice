605d857028a871e0d4265e70fda468da71f0b4b8
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-04 14:12:31 -0800

    Time: 23 ms (63.14%) | Memory: 91.7 MB (6.29%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> preSum(n + 1, 0);
+        // 计算 nums 的前缀和
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                preSum[i + 1] = preSum[i] - 1;
+            } else {
+                preSum[i + 1] = preSum[i] + 1;
+            }
+        }
+        // 前缀和到索引的映射，方便快速查找所需的前缀和
+        unordered_map<int, int> valToIndex;
+        int res = 0;
+        for (int i = 0; i < preSum.size(); i++) {
+            // 如果这个前缀和还没有对应的索引，说明这个前缀和第一次出现，记录下来
+            if (valToIndex.find(preSum[i]) == valToIndex.end()) {
+                valToIndex[preSum[i]] = i;
+            } else {
+                // 这个前缀和已经出现过了，则找到一个和为 0 的子数组
+                res = max(res, i - valToIndex[preSum[i]]);
+            }
+            // 因为题目想找长度最大的子数组，所以前缀和索引应尽可能小
+        }
+        return res;
+    }
+};
