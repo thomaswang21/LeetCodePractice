d80576d6d8546d86587c97d2cdd9447d245d97af
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-19 13:04:27 -0800

    Time: 0 ms (100.00%) | Memory: 24 MB (23.29%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -3,8 +3,8 @@ public:
     vector<int>preSum;
     NumArray(vector<int>& nums) {
         preSum.resize(nums.size()+1,0);
-        for(int i=1; i<preSum.size();i++){
-            preSum[i]=preSum[i-1]+nums[i-1];
+        for(int i=1;i<preSum.size();i++){
+            preSum[i]=nums[i-1]+preSum[i-1];
         }
     }
     
@@ -12,12 +12,6 @@ public:
         return preSum[right+1]-preSum[left];
     }
 };
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(left,right);
- */
 
 /**
  * Your NumArray object will be instantiated and called as such:
