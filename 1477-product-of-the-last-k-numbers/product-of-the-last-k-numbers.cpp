72a8fa0b3370aaf483e96b92760282f5100c58d7
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-19 22:05:02 -0800

    Time: 23 ms (73.26%) | Memory: 99.1 MB (14.24%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,28 +1,34 @@
 class ProductOfNumbers {
 public:
-    int p[40001],cnt;
+    vector<long long> p; // 用 long long 更安全
+    int cnt;
+
     ProductOfNumbers() {
-        cnt=0;
-        p[0]=1;
+        cnt = 0;
+        p.push_back(1); // p[0] = 1
     }
     
     void add(int num) {
-        if(num!=0){
-            p[cnt+1]=p[cnt]*num;
+        if (num != 0) {
+            p.push_back(p[cnt] * num);
             cnt++;
-        }else{
-            cnt=0;
+        } else {
+            // 遇到 0，清空前缀积
+            p.clear();
+            p.push_back(1);
+            cnt = 0;
         }
     }
     
     int getProduct(int k) {
-        if(k<=cnt){
-            return p[cnt]/p[cnt-k];
-        }else{
+        if (k <= cnt) {
+            return p[cnt] / p[cnt - k];
+        } else {
             return 0;
         }
     }
 };
+
 
 /**
  * Your ProductOfNumbers object will be instantiated and called as such:
