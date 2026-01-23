d6fc5907a5f4c0ddb94a8d59f9ee9474872ca6c9
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-23 15:22:17 -0800

    Time: 46 ms (31.49%) | Memory: 22.6 MB (72.08%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n=s.length();
+        unordered_set<string>seen;
+        unordered_set<string>dup;
+        for(int i=0;i+10<=n;i++){
+            string seq=s.substr(i, 10);
+        if(seen.count(seq)){
+            dup.insert(seq);
+        }
+        seen.insert(seq);
+        }
+        return vector<string>(dup.begin(),dup.end());
+    }
+};
