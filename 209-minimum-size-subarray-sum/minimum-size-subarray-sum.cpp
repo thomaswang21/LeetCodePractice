357eb623079a56cc2851454fd6fc1a79fc47288d
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-23 15:09:05 -0800

    Time: 0 ms (100.00%) | Memory: 32 MB (72.67%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int left = 0;
-        int currentSum = 0;
-        int minLength = INT_MAX; // 初始化为一个最大整数，表示还没找到
-
-        for (int right = 0; right < nums.size(); ++right) {
-            // 1. 进窗口
-            currentSum += nums[right];
-
-            // 2. 满足条件时，尝试缩小窗口
-            while (currentSum >= target) {
-                // 更新最小长度
-                int currentLen = right - left + 1;
-                if (currentLen < minLength) {
-                    minLength = currentLen;
+        int left=0;
+        int currentSum=0;
+        
+        int minLength=INT_MAX;
+        for(int right=0; right<nums.size();right++){
+            currentSum+=nums[right];
+            while(currentSum>=target){
+                int currentLength=right-left+1;
+                if(currentLength<minLength){
+                    minLength=currentLength;
                 }
-                
-                // 出窗口
-                currentSum -= nums[left];
+                currentSum-=nums[left];
                 left++;
             }
-        }
 
-        // 如果 minLength 还是初始值，说明没找到任何符合条件的子数组
-        if (minLength == INT_MAX) {
-            return 0;
         }
-        
+        if(minLength==INT_MAX)return 0;
         return minLength;
+
     }
 };
