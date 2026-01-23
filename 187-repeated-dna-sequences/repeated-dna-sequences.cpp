21f231db613f8ee975b20fa57a69b604595b1d9e
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-23 15:28:59 -0800

    Time: 43 ms (44.27%) | Memory: 22.4 MB (74.75%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -4,12 +4,12 @@ public:
         int n=s.length();
         unordered_set<string>seen;
         unordered_set<string>dup;
-        for(int i=0;i+10<=n;i++){
-            string seq=s.substr(i, 10);
-        if(seen.count(seq)){
-            dup.insert(seq);
-        }
-        seen.insert(seq);
+        for(int i=0; i+10<=n;i++){
+            string seq=s.substr(i,10);
+            if(seen.count(seq)){
+                dup.insert(seq);
+            }
+            seen.insert(seq);
         }
         return vector<string>(dup.begin(),dup.end());
     }
