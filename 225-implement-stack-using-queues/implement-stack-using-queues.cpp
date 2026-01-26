a8fc6e31fa16fcfa7e12053c938fdf29f1e39242
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-01-26 10:59:35 -0800

    Time: 0 ms (100.00%) | Memory: 9.6 MB (19.72%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -19,10 +19,11 @@ public:
         q1.pop();
         swap(q1, q2);
         return val;
+
     }
     
     int top() {
-        while(q1.size()>1){
+         while(q1.size()>1){
             q2.push(q1.front());
             q1.pop();
         }
@@ -37,15 +38,6 @@ public:
         return q1.empty()&&q2.empty();
     }
 };
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
 
 /**
  * Your MyStack object will be instantiated and called as such:
