adbb76c70bf6b8f7e509106232dff39d5c8a3cff
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-09 16:53:07 -0800

    Time: 25 ms (30.17%) | Memory: 17.3 MB (82.77%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,19 +4,32 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> res;
         int n = nums.size();
+
         for (int i = 0; i < n; i++) {
+            // 去重：第 1 个数
             if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             for (int j = i + 1; j < n; j++) {
+                // 去重：第 2 个数
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
                 int left = j + 1, right = n - 1;
+
                 while (left < right) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    long long sum = (long long)nums[i] 
+                                  + nums[j]
+                                  + nums[left]
+                                  + nums[right];
+
                     if (sum == target) {
                         res.push_back({nums[i], nums[j], nums[left], nums[right]});
                         left++;
                         right--;
+
+                        // 去重：第 3、4 个数
                         while (left < right && nums[left] == nums[left - 1]) left++;
                         while (left < right && nums[right] == nums[right + 1]) right--;
+
                     } else if (sum > target) {
                         right--;
                     } else {
@@ -25,6 +38,7 @@ public:
                 }
             }
         }
+
         return res;
     }
 };
