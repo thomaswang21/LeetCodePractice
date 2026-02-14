fdf7668177320ba5b44ccc96c64e9d82fb0d7b86
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-13 18:25:35 -0800

    Time: 40 ms (27.10%) | Memory: 64.2 MB (17.96%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        unordered_set<int>temp;
+        for(auto x:nums){
+            temp.insert(x);
+        }
+        for(int i=1; i<=n;i++){
+            if(temp.find(i)==temp.end()){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
