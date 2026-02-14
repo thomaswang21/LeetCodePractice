8482b1b85c3aefd875e6453d89179c781f0fed8a
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-13 16:46:32 -0800

    Time: 7 ms (47.75%) | Memory: 11.7 MB (36.48%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        unordered_map<char,int>cnt;
+        for(auto c:p)cnt[c]++;
+        vector<int>res;
+        int tot=cnt.size();
+        int satisfy=0;
+        for(int i=0, j=0;i< s.size();i++){
+            cnt[s[i]]--;
+            if(cnt[s[i]]==0)satisfy++;
+            while(i-j+1>p.size()){
+                if(cnt[s[j]]==0)satisfy--;
+                cnt[s[j]]++;
+                j++;
+            }
+            if(satisfy==tot){
+                res.push_back(j);
+            }
+        }
+        return res;
+    }
+};
