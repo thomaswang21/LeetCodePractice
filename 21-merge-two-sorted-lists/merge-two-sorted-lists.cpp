ec2debeccf437599c7faa9d575de945797ffb700
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-14 17:04:09 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (61.90%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -13,6 +13,7 @@ public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         auto dummy=new ListNode(-1);
         auto p=dummy;
+
         while(l1&&l2){
             if(l1->val<l2->val){
                 p->next=l1;
@@ -23,9 +24,10 @@ public:
                 l2=l2->next;
                 p=p->next;
             }
+            
         }
         if(l1)p->next=l1;
-        if(l2)p->next=l2;
+            if(l2)p->next=l2;
         return dummy->next;
     }
 };
