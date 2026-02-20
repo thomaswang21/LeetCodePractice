75d040cdc009e6223e3247adf4c215de22002436
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-20 00:49:50 -0800

    Time: 0 ms (100.00%) | Memory: 8.6 MB (19.89%) - LeetSync

--- 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.cpp
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
         vector<int> res;
-        while (label) res.push_back(label), label >>= 1;
 
-        reverse(res.begin(), res.end());
+        while (label >= 1) {
+            res.push_back(label);
 
-        for (int i = res.size() % 2; i < res.size(); i += 2) {
-            res[i] ^= ((1 << i) - 1);
+            // 计算当前层
+            int level = log2(label);
+            int start = 1 << level;
+            int end = (1 << (level + 1)) - 1;
+
+            // 关键：先镜像，再找父节点
+            label = (start + end - label) / 2;
         }
 
+        reverse(res.begin(), res.end());
         return res;
     }
 };
+
+
 
