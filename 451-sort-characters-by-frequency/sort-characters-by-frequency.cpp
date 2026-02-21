addc08664a5f67158a5b53ce23268178763038f2
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-20 17:02:43 -0800

    Time: 3 ms (55.42%) | Memory: 10.9 MB (80.29%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int>hash;
+        for(auto t: s){
+            hash[t]++;
+        }
+        priority_queue<pair<int,char>>heap;
+        for(auto x:hash){
+            heap.push({ x.second,x.first});
+        }
+        string ans = "";
+        while(!heap.empty()){
+            int cnt = heap.top().first;
+            char ch = heap.top().second;
+            for (int i = 1; i <= cnt; i++) {
+                ans += ch;
+            }
+            heap.pop();
+        }
+        return ans;
+
+    }
+};
