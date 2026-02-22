6a0063a951352d59662e3347e316fa70a9d91a5e
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-22 02:46:32 -0800

    Time: 405 ms (16.11%) | Memory: 167.8 MB (26.70%) - LeetSync

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -8,16 +8,27 @@
  * };
  */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
-TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-    if (original == nullptr) return nullptr;
-    if (original == target) return cloned;
-    
-    TreeNode* left = getTargetCopy(original->left, cloned->left, target);
-    if (left) return left;
-    
-    return getTargetCopy(original->right, cloned->right, target);
-}
-
+   TreeNode* ans;
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) 
+    {
+        if (cloned == NULL)
+            return cloned;
+        if (cloned->val == target->val) // If target node found in cloned tree save it into a variable.
+            ans = cloned;
+        getTargetCopy(original, cloned->left, target);
+        getTargetCopy(original, cloned->right, target);
+        return ans;
+    }
 };
