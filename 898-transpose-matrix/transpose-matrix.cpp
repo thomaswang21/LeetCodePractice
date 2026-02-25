13dee4843012268cbacebc6d2c84cdc784277e37
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-25 10:00:31 -0800

    Time: 0 ms (100.00%) | Memory: 15.2 MB (6.48%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<vector<int>> res(cols, vector<int>(rows));
-        
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                res[c][r] = matrix[r][c]; // 正确赋值
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        vector<vector<int>>res(cols, vector<int>(rows));
+        for(int i=0; i<rows;i++){
+            for(int j=0;j<cols;j++){
+                res[j][i]=matrix[i][j];
             }
         }
         return res;
