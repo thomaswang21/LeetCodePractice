663a69b113477210db665716de4bca5dbe683ff9
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-25 15:01:03 -0800

    Time: 0 ms (100.00%) | Memory: 19 MB (47.69%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -9,25 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-
-
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        // 如果节点为空，深度为0
-        if (root == nullptr) {
-            return 0;
-        }
-        // 递归计算左右子树的深度
-        int leftDepth = maxDepth(root->left);
-        int rightDepth = maxDepth(root->right);
-        // 返回较大深度加1
-        if (leftDepth > rightDepth) {
-            return leftDepth + 1;
-        } else {
-            return rightDepth + 1;
-        }
-    }
-};
+    if (!root ) return 0;
 
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return max(leftDepth, rightDepth) + 1;
+}
+
+};
