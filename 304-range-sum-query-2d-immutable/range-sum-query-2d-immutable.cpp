4b08a8112914db4a44b1bff64dade7b9c0cf7fc3
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-25 13:18:23 -0800

    Time: 23 ms (61.54%) | Memory: 149.1 MB (59.38%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,27 +1,34 @@
-
-using namespace std;
-
 class NumMatrix {
-private:
-    vector<vector<int>> prefix; // 二维前缀和数组
+
+    vector<vector<int>> A;
+    int m, n;
+
 public:
     NumMatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        prefix.assign(m + 1, vector<int>(n + 1, 0));
+        m = matrix.size();
+        n = matrix[0].size();
+        A.resize(m+1, vector<int>(n+1, 0));
 
-        // 构建前缀和
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                prefix[i + 1][j + 1] = prefix[i][j + 1] + prefix[i + 1][j] - prefix[i][j] + matrix[i][j];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                A[i][j] = matrix[i-1][j-1] + A[i-1][j] + A[i][j-1] - A[i-1][j-1];
             }
         }
     }
-
+    
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return prefix[row2 + 1][col2 + 1]
-             - prefix[row1][col2 + 1]
-             - prefix[row2 + 1][col1]
-             + prefix[row1][col1];
+        return A[row2+1][col2+1] - A[row1][col2+1] - A[row2+1][col1] + A[row1][col1];
     }
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
