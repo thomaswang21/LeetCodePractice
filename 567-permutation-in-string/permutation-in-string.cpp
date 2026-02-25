a8f8c646e49220b97995842579627e64be2f04a0
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-02-25 13:51:42 -0800

    Time: 3 ms (66.86%) | Memory: 10 MB (69.76%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,38 +1,35 @@
+
+
+#include <vector>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
-    // 判断 s 中是否存在 t 的排列
-    bool checkInclusion(string t, string s) {
-        unordered_map<char, int> need, window;
-        for (char c : t) need[c]++;
+    bool checkInclusion(string s1, string s2) {
+        int len1 = s1.size();
+        int len2 = s2.size();
+        if (len1 > len2) return false;
 
-        int left = 0, right = 0;
-        int valid = 0;
-        while (right < s.size()) {
-            char c = s[right];
-            right++;
-            // 进行窗口内数据的一系列更新
-            if (need.count(c)) {
-                window[c]++;
-                if (window[c] == need[c])
-                    valid++;
-            }
+        vector<int> count1(26, 0), count2(26, 0);
 
-            // 判断左侧窗口是否要收缩
-            while (right - left >= t.size()) {
-                // 在这里判断是否找到了合法的子串
-                if (valid == need.size())
-                    return true;
-                char d = s[left];
-                left++;
-                // 进行窗口内数据的一系列更新
-                if (need.count(d)) {
-                    if (window[d] == need[d])
-                        valid--;
-                    window[d]--;
-                }
-            }
+        // 初始化频率数组
+        for (int i = 0; i < len1; i++) {
+            count1[s1[i] - 'a']++;
+            count2[s2[i] - 'a']++;
         }
-        // 未找到符合条件的子串
+
+        // 判断初始窗口
+        if (count1 == count2) return true;
+
+        // 滑动窗口
+        for (int i = len1; i < len2; i++) {
+            count2[s2[i] - 'a']++;           // 加入新字符
+            count2[s2[i - len1] - 'a']--;    // 移除旧字符
+            if (count1 == count2) return true;
+        }
+
         return false;
     }
 };
+
