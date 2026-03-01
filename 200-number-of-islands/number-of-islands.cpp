9da876288f298c035c67753623720dc877170109
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-01 11:26:03 -0800

    Time: 23 ms (83.59%) | Memory: 16.7 MB (58.41%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int res=0;
-        int row=grid.size();
-        int cols=grid[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]=='1'){
+        if(grid.empty() || grid[0].empty()) return 0;
+        
+        int res = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false)); // 访问标记数组
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1' && !visited[i][j]) {
                     res++;
-                    dfs(grid, i,j);
+                    dfs(grid, i, j, visited);
                 }
             }
         }
         return res;
     }
-    void dfs(vector<vector<char>>&grid, int i, int j){
-        int m=grid.size(), n=grid[0].size();
-        if(i<0||j<0||i>=m||j>=n){
-            return;
-        }
-       if (grid[i][j]=='0')return;
-        grid[i][j]='0';
-        dfs(grid, i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
 
+    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited) {
+        int m = grid.size();
+        int n = grid[0].size();
+        // 边界条件 + 已访问或水
+        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || grid[i][j] == '0') 
+            return;
+
+        visited[i][j] = true; // 标记为已访问
+
+        // 上下左右递归
+        dfs(grid, i + 1, j, visited);
+        dfs(grid, i - 1, j, visited);
+        dfs(grid, i, j + 1, visited);
+        dfs(grid, i, j - 1, visited);
     }
 };
