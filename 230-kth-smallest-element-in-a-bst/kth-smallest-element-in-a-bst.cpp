2e5eff5a13fb82675e3453556a73d797e0724a41
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-02 03:39:55 -0800

    Time: 0 ms (100.00%) | Memory: 24.6 MB (27.58%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,30 +1,16 @@
 class Solution {
-private:
-    // 记录结果
-    int res = 0;
-    // 记录当前元素的排名
-    int rank = 0;
-    void traverse(TreeNode* root, int k) {
-        if (root == nullptr) {
-            return;
-        }
-        traverse(root->left, k);
-
-        // 中序代码位置
-        rank++;
-        if (k == rank) {
-            // 找到第 k 小的元素
-            res = root->val;
-            return;
-        }
-
-        traverse(root->right, k);
-    }
-
 public:
+    void inOrderTraversal(TreeNode* root, vector<int> &v){
+        if(root == NULL)    return;
+        
+        //left, root, right 
+        inOrderTraversal(root->left, v);
+        v.push_back(root->val);
+        inOrderTraversal(root->right, v);      
+    }
     int kthSmallest(TreeNode* root, int k) {
-        // 利用 BST 的中序遍历特性
-        traverse(root, k);
-        return res;
+        vector<int> v; 
+        inOrderTraversal(root, v);
+        return v[k-1];
     }
 };
