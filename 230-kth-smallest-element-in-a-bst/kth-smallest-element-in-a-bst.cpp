a6462d91a34d77ba640d1005f543bac8089b91a2
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-02 03:37:52 -0800

    Time: 0 ms (100.00%) | Memory: 24.2 MB (89.07%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,31 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-public:
-    int k, ans;
-    int kthSmallest(TreeNode* root, int _k) {
-       if(!root)return 0;
-        k=_k;
-        dfs(root);
-        return ans;
-
-    }
-    bool dfs(TreeNode*root){
-        if(!root)return false;
-        if(dfs(root->left))return true;
-        if(--k==0){
-            ans=root->val;
-            return true;
+private:
+    // 记录结果
+    int res = 0;
+    // 记录当前元素的排名
+    int rank = 0;
+    void traverse(TreeNode* root, int k) {
+        if (root == nullptr) {
+            return;
         }
-        return dfs(root->right);
+        traverse(root->left, k);
+
+        // 中序代码位置
+        rank++;
+        if (k == rank) {
+            // 找到第 k 小的元素
+            res = root->val;
+            return;
+        }
+
+        traverse(root->right, k);
+    }
+
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        // 利用 BST 的中序遍历特性
+        traverse(root, k);
+        return res;
     }
 };
