f68e9d187fc0aa9465d8982a54e06c454439f075
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-03 14:05:17 -0800

    Time: 83 ms (78.06%) | Memory: 13.5 MB (86.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,19 +2,28 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int totalSum = 0;
-        for (int num : nums) totalSum += num;
-
-        if (totalSum % 2 != 0) return false;
-
-        int targetSum = totalSum / 2;
-        vector<bool> dp(targetSum + 1, false);
-        dp[0] = true;
         for (int num : nums) {
-            for (int currSum = targetSum; currSum >= num; --currSum) {
-                dp[currSum] = dp[currSum] || dp[currSum - num];
-                if (dp[targetSum]) return true;
+            totalSum += num;
+        }
+
+        // If total sum is odd, we can't divide it into two equal integer subsets
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        int target = totalSum / 2;
+        
+        // dp[i] will be true if a sum of i can be achieved
+        vector<bool> dp(target + 1, false);
+        dp[0] = true; // Base case: sum 0 is always possible
+
+        for (int num : nums) {
+            // Iterate backwards to avoid using the same element twice
+            for (int j = target; j >= num; --j) {
+                dp[j] = dp[j] || dp[j - num];
             }
         }
-        return dp[targetSum];
+
+        return dp[target];
     }
 };
