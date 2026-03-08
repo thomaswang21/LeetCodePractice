bd6f9185e68b53282c8354c668bc588fa35786db
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-08 14:17:22 -0700

    Time: 2 ms (94.19%) | Memory: 13.3 MB (41.72%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,32 @@
+
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // 初始化边界
+        for (int i = 0; i <= n; ++i) dp[i][0] = i;
+        for (int j = 0; j <= m; ++j) dp[0][j] = j;
+
+        // 填表
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int del = dp[i - 1][j] + 1;     // 删除
+                    int ins = dp[i][j - 1] + 1;     // 插入
+                    int rep = dp[i - 1][j - 1] + 1; // 替换
+                    dp[i][j] = min(del, min(ins, rep));
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
+
+
