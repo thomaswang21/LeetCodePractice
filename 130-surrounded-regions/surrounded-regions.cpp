8ee2238112bebe5e705c869b9ee46a441b80b540
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-10 19:29:34 -0700

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.47%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,45 +1,46 @@
+
+
 class Solution {
 public:
-    int n, m;
-    vector<vector<bool>> st;
-
     void solve(vector<vector<char>>& board) {
-        n = board.size();
-        m = board[0].size();
-        st = vector<vector<bool>>(n, vector<bool>(m, false));
+        // 如果为空直接返回
+        if (board.empty() || board[0].empty()) return;
 
-        // 从四条边的 'O' 开始 DFS
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O') dfs(i, 0, board);
-            if (board[i][m - 1] == 'O') dfs(i, m - 1, board);
-        }
-        for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O') dfs(0, j, board);
-            if (board[n - 1][j] == 'O') dfs(n - 1, j, board);
-        }
+        int m = board.size();
+        int n = board[0].size();
 
-        // 把不安全的 O 变成 X
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!st[i][j] && board[i][j] == 'O') {
-                    board[i][j] = 'X';
-                }
+        // 定义四个方向向量（二维）
+        vector<pair<int, int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        // 定义DFS函数
+        function<void(int,int)> dfs = [&](int x, int y) {
+            // 越界或非 'O' 直接返回
+            if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O') return;
+            // 标记为安全区域
+            board[x][y] = '#';
+            // 向四个方向扩展
+            for (auto& d : directions) {
+                dfs(x + d.first, y + d.second);
             }
+        };
+
+        // 从边界的 'O' 开始DFS
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(i, 0);
+            if (board[i][n-1] == 'O') dfs(i, n-1);
         }
-    }
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(0, j);
+            if (board[m-1][j] == 'O') dfs(m-1, j);
+        }
 
-    void dfs(int x, int y, vector<vector<char>>& board) {
-        st[x][y] = true;
-
-        static int dirs[4][2] = {{-1,0},{1,0},{0,1},{0,-1}};
-
-        for (auto& d : dirs) {
-            int a = x + d[0];
-            int b = y + d[1];
-            if (a >= 0 && a < n && b >= 0 && b < m 
-                && !st[a][b] && board[a][b] == 'O') {
-                dfs(a, b, board);
+        // 遍历整个矩阵，修改字符
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X'; // 被包围的区域
+                else if (board[i][j] == '#') board[i][j] = 'O'; // 安全区域恢复
             }
         }
     }
 };
+
