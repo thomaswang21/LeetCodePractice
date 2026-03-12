1bba33c9c733b80bb2ee8261bac1f8a446da02b4
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-11 17:36:13 -0700

    Time: 36 ms (84.97%) | Memory: 84.5 MB (24.82%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,45 +1,31 @@
-#include <vector>
-#include <algorithm>
+
 
 class Solution {
 public:
-    int maxEnvelopes(std::vector<std::vector<int>>& envelopes) {
-        int n = envelopes.size();
-        // 按宽度升序排列，如果宽度一样，则按高度降序排列
-        std::sort(envelopes.begin(), envelopes.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
-            if (a[0] == b[0]) {
-                return b[1] < a[1]; // 宽度相同，高度降序
-            } else {
-                return a[0] < b[0]; // 宽度升序
-            }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        // 按宽度升序，高度降序排序
+        sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b) {
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
         });
 
-        // 提取高度数组
-        std::vector<int> height(n);
-        for (int i = 0; i < n; i++)
-            height[i] = envelopes[i][1];
-
-        return lengthOfLIS(height);
-    }
-
-    // 返回 nums 中 LIS 的长度（O(n log n) 方法）
-    int lengthOfLIS(std::vector<int>& nums) {
-        int piles = 0, n = nums.size();
-        std::vector<int> top(n);
-        for (int i = 0; i < n; i++) {
-            int poker = nums[i];
-            int left = 0, right = piles;
-            // 二分查找
-            while (left < right) {
-                int mid = (left + right) / 2;
-                if (top[mid] >= poker)
-                    right = mid;
-                else
-                    left = mid + 1;
-            }
-            if (left == piles) piles++;
-            top[left] = poker;
+        // 构建高度序列
+        vector<int> heights;
+        for (int i = 0; i < envelopes.size(); i++) {
+            heights.push_back(envelopes[i][1]);
         }
-        return piles;
+
+        // 寻找高度的最长递增子序列
+        vector<int> lis;
+        for (int i = 0; i < heights.size(); i++) {
+            int h = heights[i];
+            auto it = lower_bound(lis.begin(), lis.end(), h);
+            if (it == lis.end()) {
+                lis.push_back(h);
+            } else {
+                *it = h;
+            }
+        }
+        return lis.size();
     }
 };
