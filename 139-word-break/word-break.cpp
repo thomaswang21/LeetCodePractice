9dd4cbe137eb3a661f2c70d302b3170f436c4966
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-12 11:46:45 -0700

    Time: 15 ms (28.64%) | Memory: 15.9 MB (49.73%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,38 +1,32 @@
-#include <vector>
-#include <string>
-#include <unordered_set>
 
-using namespace std;
 
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        // Use a set for O(1) average time complexity lookups
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        
-        int n = s.length();
-        
-        // dp[i] represents if s.substr(0, i) can be segmented
-        // Size is n + 1 to accommodate the empty string (dp[0])
-        vector<bool> dp(n + 1, false);
-        
-        // Base case: empty string is valid
-        dp[0] = true;
-        
-        // Iterate through the string length i
-        for (int i = 1; i <= n; ++i) {
-            // Iterate through all possible previous split points j
-            for (int j = 0; j < i; ++j) {
-                
-                // If the first part (0 to j) is valid 
-                // AND the substring from j to i is in the dictionary
-                if (dp[j] && dict.count(s.substr(j, i - j))) {
-                    dp[i] = true;
-                    break; // No need to check other j's if we found a valid path
+        // 将字典放入哈希表，方便 O(1) 查询
+        unordered_set<string> dict;
+        for (int i = 0; i < wordDict.size(); i++) {
+            dict.insert(wordDict[i]); // 插入字典单词
+        }
+
+        int n = s.size();
+        vector<bool> dp(n + 1, false); // dp[i] 表示前 i 个字符是否可拆分
+        dp[0] = true; // 空字符串可被拆分
+
+        // 遍历字符串
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                // 如果前 j 个能拆分，且 s[j..i-1] 在字典中
+                if (dp[j]) {
+                    string word = s.substr(j, i - j); // 取子串
+                    if (dict.find(word) != dict.end()) {
+                        dp[i] = true; // 前 i 个字符可拆分
+                        break; // 提前结束，减少无效判断
+                    }
                 }
             }
         }
-        
-        return dp[n];
+
+        return dp[n]; // 返回结果
     }
 };
