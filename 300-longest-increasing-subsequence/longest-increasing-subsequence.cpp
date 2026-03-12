1a547302b68bd9776e0df127603fb9c2436d4f64
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-11 17:05:49 -0700

    Time: 84 ms (41.94%) | Memory: 14.3 MB (60.45%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,29 @@
 
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
+        if (n == 0) return 0;
 
-        vector<int> dp(n, 1);
+        vector<int> dp(n, 1); // 每个元素至少能构成长度为1的递增子序列
+        int ans = 1;
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<i; j++) {
-                if(nums[i] > nums[j]) {
-                    dp[i] = max(dp[i], dp[j]+1);
+        // 双层循环计算每个位置的最长递增子序列长度
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) { // 如果前面的数比当前数小
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1; // 更新dp[i]
+                    }
                 }
+            }
+            if (ans < dp[i]) {
+                ans = dp[i]; // 更新全局最大值
             }
         }
 
-            int res = 0;
-            for (int x : dp) {
-                res = max(res, x);
-            }
-            return res;
-                }
-}; 
+        return ans;
+    }
+};
+
