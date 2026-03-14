070fd4c62ea5f2a527aad642199fd81249a47675
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-14 10:49:20 -0700

    Time: 6 ms (46.41%) | Memory: 24.9 MB (12.71%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,37 +1,15 @@
-
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // 如果区间为空，直接返回空结果
-        if (intervals.empty()) return {};
-
-        // 按区间起点排序
         sort(intervals.begin(), intervals.end());
-
-        // 结果数组
-        vector<vector<int>> merged;
-        // 将第一个区间加入结果
-        merged.push_back(intervals[0]);
-
-        // 遍历剩余区间
-        for (int i = 1; i < intervals.size(); i++) {
-            // 获取结果中最后一个区间的引用
-            vector<int>& last = merged.back();
-
-            // 如果当前区间的起点 <= 上一个区间的终点，说明重叠
-            if (intervals[i][0] <= last[1]) {
-                // 更新终点为两者中较大的那个
-                last[1] = max(last[1], intervals[i][1]);
-            } else {
-                // 否则直接加入结果
-                merged.push_back(intervals[i]);
+        vector<vector<int>>res;
+        for(auto a: intervals){
+            if(res.empty()||a[0]>res.back()[1]){
+                res.push_back(a);
+            }else{
+                res.back()[1]=max(res.back()[1], a[1]);
             }
         }
-
-        return merged;
+        return res;
     }
 };
-
-
-
