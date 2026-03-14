3aa8e9e90cbab05ba8c8a453c1db6f0f30afca8d
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-14 09:36:55 -0700

    Time: 2 ms (32.90%) | Memory: 21.8 MB (46.79%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& b) {
-        vector<vector<int>>res;
-        int k=0;
-        while(k<a.size()&&a[k][1]<b[0]){
-            res.push_back(a[k]);
-            k++;
-        }
-        if(k<a.size()){
-            b[0]=min(b[0], a[k][0]);
-            while(k<a.size()&&a[k][0]<=b[1]){
-                b[1]=max(a[k][1],b[1]);
-                k++;
-            }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int i = 0;
+        int n = intervals.size();
 
+        // 1. 左边不重叠
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
         }
-        res.push_back(b);
-        while(k<a.size()){
-            res.push_back(a[k]);
-            k++;
+
+        // 2. 合并重叠
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
+
+        res.push_back(newInterval);
+
+        // 3. 右边不重叠
+        while (i < n) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
         return res;
-
     }
 };
