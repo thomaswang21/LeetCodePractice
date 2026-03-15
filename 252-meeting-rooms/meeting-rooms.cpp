47951740152b66a9611e85349e4b65a18811418a
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-14 17:40:19 -0700

    Time: 3 ms (38.90%) | Memory: 16 MB (53.36%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.cpp
+++ 252-meeting-rooms/meeting-rooms.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        // 关键修复：处理边界情况
-        if (intervals.empty()) {
-            return true;
-        }
-
+        if(intervals.empty())return true;
         sort(intervals.begin(), intervals.end());
-        
-        int n = intervals.size();
-        int end = intervals[0][1];
-        
-        for (int i = 1; i < n; i++) {
-            // 如果当前会议的开始时间 < 上一个会议的结束时间，说明重叠
-            if (intervals[i][0] < end) {
+        int end=intervals[0][1];
+        for(int i=1; i<intervals.size();i++){
+            if(intervals[i][0]<end){
                 return false;
-            } else {
-                // 更新当前的最晚结束时间
-                // 注意：由于已经排过序，直接取当前会议结束时间即可
-                end = intervals[i][1];
+            }else{
+                end=intervals[i][1];
             }
         }
         return true;
