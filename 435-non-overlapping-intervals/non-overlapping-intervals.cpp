4ebadc704ff8f54e3b7eeb4d92383935eee55a45
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-15 19:43:41 -0700

    Time: 47 ms (48.86%) | Memory: 94 MB (36.73%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return 0;
-
-        // 【统一排序】按左端点升序，左相同按右降序
+        if(intervals.empty()) return 0;
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
-            if (a[0] == b[0]) return a[1] > b[1];
+            if(a[0]==b[0]) return a[1] > b[1];
             return a[0] < b[0];
         });
-
         int count = 0;
         int end = intervals[0][1];
-
-        for (int i = 1; i < intervals.size(); i++) {
-            // 发现重叠
-            if (intervals[i][0] < end) {
-                count++; // 必须删掉一个
-                // 【核心改变】贪心策略：保留结束比较早的那个区间，更新较小的 end
-                end = min(end, intervals[i][1]); 
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals[i][0] < end){
+                count++;
+                end = min(end, intervals[i][1]);
             } else {
-                // 没有重叠，正常推进右边界
                 end = intervals[i][1];
             }
         }
-
         return count;
     }
 };
-
