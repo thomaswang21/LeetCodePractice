4d01bea2ba0d1084749ca5a7ac1d4ad7ad5edf30
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-16 00:06:06 -0700

    Time: 0 ms (100.00%) | Memory: 27.4 MB (39.78%) - LeetSync

--- 955-complete-binary-tree-inserter/complete-binary-tree-inserter.cpp
+++ 955-complete-binary-tree-inserter/complete-binary-tree-inserter.cpp
@@ -1,46 +1,50 @@
+
+
 class CBTInserter {
-    // 这个队列只记录完全二叉树底部可以进行插入的节点
-    private:
-        queue<TreeNode*> q;
-        TreeNode* root;
+private:
+    TreeNode* root; // 根节点
+    queue<TreeNode*> q; // 队列用于存储未满的节点
 
-    public:
-        CBTInserter(TreeNode* root) {
-            this->root = root;
-            // 进行普通的 BFS，目的是找到底部可插入的节点
-            queue<TreeNode*> temp;
-            temp.push(root);
-            while (!temp.empty()) {
-                TreeNode* cur = temp.front(); temp.pop();
-                if (cur->left != nullptr) {
-                    temp.push(cur->left);
-                }
-                if (cur->right != nullptr) {
-                    temp.push(cur->right);
-                }
-                if (cur->right == nullptr || cur->left == nullptr) {
-                    // 找到完全二叉树底部可以进行插入的节点
-                    q.push(cur);
-                }
+public:
+    // 构造函数，初始化时进行层序遍历，找到所有未满的节点
+    CBTInserter(TreeNode* root) {
+        this->root = root; // 保存根节点
+        queue<TreeNode*> tmp; // 临时队列用于层序遍历
+        tmp.push(root); // 根节点入队
+        while (!tmp.empty()) {
+            TreeNode* node = tmp.front(); // 取队首节点
+            tmp.pop(); // 出队
+            // 如果该节点的左右孩子不全，则加入q队列
+            if (!(node->left && node->right)) {
+                q.push(node);
             }
+            // 左右孩子入队继续遍历
+            if (node->left) tmp.push(node->left);
+            if (node->right) tmp.push(node->right);
         }
+    }
 
-        int insert(int val) {
-            TreeNode* node = new TreeNode(val);
-            TreeNode* cur = q.front();
-            // 进行插入
-            if (cur->left == nullptr) {
-                cur->left = node;
-            } else if (cur->right == nullptr) {
-                cur->right = node;
-                q.pop();
-            }
-            // 新节点的左右节点也是可以插入的
-            q.push(node);
-            return cur->val;
+    // 插入新节点
+    int insert(int val) {
+        TreeNode* node = new TreeNode(val); // 创建新节点
+        TreeNode* parent = q.front(); // 队首节点为父节点
+        // 如果父节点没有左孩子，则插入左边
+        if (!parent->left) {
+            parent->left = node;
+        } 
+        // 否则插入右边，并弹出该父节点
+        else {
+            parent->right = node;
+            q.pop();
         }
+        // 新节点也可能成为未来的父节点，入队
+        q.push(node);
+        // 返回父节点的值
+        return parent->val;
+    }
 
-        TreeNode* get_root() {
-            return root;
-        }
+    // 返回根节点
+    TreeNode* get_root() {
+        return root;
+    }
 };
