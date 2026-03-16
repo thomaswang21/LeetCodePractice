ecff3250f41fbbc64a833a3711c510523fa0a7f3
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-16 15:03:52 -0700

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.40%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
-        vector<int>F(n+1,0);
-        F[0]=0;
-        F[1]=1;
-        for(int i=2; i<=n;i++){
-            F[i]=F[i-1]+F[i-2];
+        int res;
+        vector<int> dp(n+1, 0);
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return F[n];
+        return dp[n];
     }
 };
