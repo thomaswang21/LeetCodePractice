3dcf664afb813f735f22d1d929ddeca9a1e8e449
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-03-21 06:22:51 -0700

    Time: 1 ms (48.61%) | Memory: 19.4 MB (74.16%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,24 +1,64 @@
-class Solution {
-public:
-    vector<int> p;
 
-    int find(int x) {
-        if (p[x] != x) p[x] = find(p[x]);
-        return p[x];
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rankv;
+
+    // 构造函数 O(n)
+    DSU(int n) {
+        parent.resize(n);
+        rankv.resize(n, 0);
+        for (int i = 0; i < n; i++) parent[i] = i;
     }
 
-    int findCircleNum(vector<vector<int>>& M) {
-        int n = M.size();
-        for (int i = 0; i < n; i ++ ) p.push_back(i);
+    // 查找根节点 O(α(n))
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // 路径压缩
+        }
+        return parent[x];
+    }
 
-        int cnt = n;
-        for (int i = 0; i < n; i ++ )
-            for (int j = 0; j < n; j ++ )
-                if (M[i][j] && find(i) != find(j)) {
-                    p[find(i)] = find(j);
-                    cnt -- ;
-                }
-
-        return cnt;
+    // 合并两个集合 O(α(n))
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px != py) {
+            if (rankv[px] < rankv[py]) {
+                parent[px] = py;
+            } else if (rankv[px] > rankv[py]) {
+                parent[py] = px;
+            } else {
+                parent[py] = px;
+                rankv[px]++;
+            }
+        }
     }
 };
+
+class Solution {
+public:
+    // 主函数 O(n^2 * α(n))
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DSU dsu(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    dsu.unite(i, j);
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (dsu.find(i) == i) count++;
+        }
+        return count;
+    }
+};
+
