b320050fc207380ea1a81800ee91847d12fd4e56
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-07-05 14:28:52 +0800

    Time: 8 ms (83.34%) | Memory: 11.9 MB (53.76%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,12 +12,11 @@ public:
         ListNode*fast=head;
         ListNode*slow=head;
         while(fast&&fast->next){
-            slow=slow->next;
             fast=fast->next->next;
-            if(fast==slow){
-                return true;
-            }
+            slow=slow->next;
+            if(fast==slow)return true;
         }
         return false;
+
     }
 };
