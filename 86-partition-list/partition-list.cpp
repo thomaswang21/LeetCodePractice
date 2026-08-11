3a90ebbe7ac2a9e5bb93b64eceff90cc4eb8e8c9
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-08-11 17:26:59 +0800

    Time: 0 ms (100.00%) | Memory: 15 MB (11.06%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -11,26 +11,23 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode*dummy1=new ListNode(-1);
-        ListNode*dummy2=new ListNode(-1);
-        ListNode*p1=dummy1;
-        ListNode*p2=dummy2;
+        if(!head) return NULL;
+        auto dummy1 = new ListNode(-1);
+        auto dummy2 = new ListNode(-1);
+        auto p1 = dummy1;
+        auto p2 = dummy2;
         while(head){
-            if(head->val<x){
-                p1->next=head;
-                
-                p1=p1->next;
+            if(head->val < x){
+                p1->next = head;
+                p1 = p1->next;
             }else{
-                p2->next=head;
-                
-                p2=p2->next;
+                p2->next = head;
+                p2 = p2->next;
             }
-            head=head->next;
+            head = head->next;
         }
-        p1->next=dummy2->next;
-        p2->next=NULL;
+        p1->next = dummy2->next;
+        p2->next = NULL;
         return dummy1->next;
-        delete dummy1;
-        delete dummy2;
-        }
+    }
 };
