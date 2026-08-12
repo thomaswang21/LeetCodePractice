a0cc8bc7a51750d146b775fd8b350196bda254b0
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-08-12 08:41:16 +0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (28.32%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,27 +11,20 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // 1. 修正语法：实例化 ListNode 对象，并用 dummy 记住头节点位置
-        ListNode* dummy = new ListNode(-1); 
-        ListNode* p = dummy; // 用 p 来往后遍历
-        
+        auto dummy = new ListNode(-1);
+        auto p = dummy;
         while(list1 && list2){
             if(list1->val < list2->val){
                 p->next = list1;
-                list1 = list1->next; // 2. 必须让 list1 往后移动
+                list1 = list1->next;
             }else{
                 p->next = list2;
-                list2 = list2->next; // 2. 必须让 list2 往后移动
+                list2 = list2->next;
             }
-            p = p->next; // 3. p 也必须往后移动，准备拼接下一个节点
+            p=p->next;
         }
-        
-        // 4. 修正指针判断语法，直接用 if(list1) 即可
         if(list1) p->next = list1;
         if(list2) p->next = list2;
-        
-        
-        return dummy->next; 
-        
+        return dummy->next;
     }
 };
