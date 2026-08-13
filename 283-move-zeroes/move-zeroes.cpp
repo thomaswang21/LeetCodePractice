f9c8f808c98d0e1ed8b309696e9869790ce377a4
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-08-13 19:36:47 +0800

    Time: 0 ms (100.00%) | Memory: 23.9 MB (56.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int k=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-            nums[k]=nums[i];
-            k++;
+        int k = 0;
+        for(int i =0; i< nums.size(); i++){
+            if(nums[i] != 0){
+                nums[k] = nums[i];
+                k++;
             }
         }
-        while(k<nums.size())nums[k++]=0;
+       while(k < nums.size()){
+        nums[k] = 0;
+        k++;
+       }
     }
 };
