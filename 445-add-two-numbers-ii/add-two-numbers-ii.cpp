2f1286e765d728dbaf59b7c61d4b7e08c85175b3
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-08-27 14:41:01 +0800

    Time: 2 ms (53.93%) | Memory: 77.1 MB (51.22%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -11,31 +11,34 @@
 class Solution {
 public:
     ListNode* reverse(ListNode*head){
-        if(!head)return head;
-            auto a=head, b=head->next;
-            while(b){
-                auto c=b->next;
-                b->next=a;
-                a=b;
-                b=c;
-
-            }
-            head->next=NULL;
-            return a;
+        if(!head) return NULL;
+        auto a = head;
+        auto b = head->next;
+        while(b){
+            auto c = b->next;
+            b->next = a;
+            a = b;
+            b = c;
         }
+        head->next = NULL;
+        return a;
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        auto dummy=new ListNode(-1);
-        auto p=dummy;
-        l1=reverse(l1), l2=reverse(l2);
-        int carry=0;
-        while(l1||l2||carry){
-            if(l1)carry+=l1->val, l1=l1->next;
-            if(l2)carry+=l2->val, l2=l2->next;
-            p->next=new ListNode(carry%10);
-            p=p->next;
-            carry/=10;
+        auto dummy = new ListNode(-1);
+        auto p = dummy;
+        int carry = 0;
+        auto l11 = reverse(l1);
+        
+        auto l22 = reverse(l2);
+        while(l11 || l22 ||carry){
+            if(l11)carry += l11->val, l11 = l11->next;;
+            
+            if(l22)carry += l22->val, l22 =l22->next;
+            ;
+            p->next = new ListNode(carry%10);
+            p = p->next;
+            carry /=10;
         }
         return reverse(dummy->next);
-        
     }
 };
