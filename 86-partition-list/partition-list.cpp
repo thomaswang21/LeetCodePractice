370c1007f4d2c8647075b9a40d4dbd0b064ee049
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-08-31 15:32:07 +0800

    Time: 0 ms (100.00%) | Memory: 14.9 MB (45.64%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -17,18 +17,17 @@ public:
         auto p1 = dummy1;
         auto p2 = dummy2;
         while(head){
-            if(head->val < x){
+            if(head->val<x){
                 p1->next = head;
                 p1 = p1->next;
             }else{
                 p2->next = head;
-                p2 = p2->next;
+                p2 =p2->next;
             }
             head = head->next;
         }
         p1->next = dummy2->next;
         p2->next = NULL;
         return dummy1->next;
-
     }
 };
