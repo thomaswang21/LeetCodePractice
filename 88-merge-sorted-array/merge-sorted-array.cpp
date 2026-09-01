632779c21c0f5f5aefc32d380c6611199a461ae3
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-01 20:24:30 +0800

    Time: 0 ms (100.00%) | Memory: 12.4 MB (48.45%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,31 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // Pointer for the last valid element in nums1
-        int p1 = m - 1;
-        // Pointer for the last element in nums2
-        int p2 = n - 1;
-        // Pointer for the last position in the merged array (nums1)
-        int p = m + n - 1;
-
-        // While both arrays have elements to compare
-        while (p1 >= 0 && p2 >= 0) {
-            if (nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1];
-                p1--;
-            } else {
-                nums1[p] = nums2[p2];
-                p2--;
-            }
-            p--;
+        for (int j = 0, i = m; j<n; j++){
+            nums1[i] = nums2[j];
+            i++;
         }
-
-        // If there are still elements left in nums2, copy them over.
-        // (If elements are left in nums1, they are already in the correct place)
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2];
-            p2--;
-            p--;
-        }
+        sort(nums1.begin(),nums1.end());
     }
 };
