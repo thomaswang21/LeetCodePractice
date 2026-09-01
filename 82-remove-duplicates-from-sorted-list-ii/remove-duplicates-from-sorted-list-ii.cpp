88b68a6ba608fc422de843b917f3bbd86de5db93
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-01 17:18:33 +0800

    Time: 0 ms (100.00%) | Memory: 15.6 MB (72.81%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -17,7 +17,7 @@ public:
         auto p = dummy;
         while(p->next && p->next->next){
             auto q = p->next->next;
-            while(q && q->val == p->next->val)q = q->next;
+            while(q && q->val == p->next->val) q = q->next;
             if(p->next->next == q)p = p->next;
             else p->next = q;
         }
