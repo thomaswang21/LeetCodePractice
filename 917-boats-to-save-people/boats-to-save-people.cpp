7357a831a1ebd95edabec97c2a3dbfa2ad711fd2
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-02 20:27:08 +0800

    Time: 18 ms (53.52%) | Memory: 45.8 MB (71.43%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,32 +1,19 @@
-				// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-       
-        // sort vector
-        sort(people.begin(),people.end());
-        
-        int i = 0, j = people.size() - 1,cnt = 0;
-        
-        while(i <= j)
-        {   
-            // lightest person + heaviest person sum <= limit
-            // they can go together
-            if(people[i] + people[j] <= limit)
-            {
-                ++i;
-                --j;
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size() - 1;
+        int cnt = 0;
+        while(i <= j){
+            if(people[i] + people[j] <= limit){
+                i++;
+                j--;
+            }else{
+                j--;
+
             }
-            // if sum is over the limit,
-            // heaviest will go alone.
-            else
-                --j;
-            
-            ++cnt;  // number of boats
+            cnt++;
         }
-        
         return cnt;
-        
     }
-	// for github repository link go to my profile.
 };
