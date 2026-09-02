3ec478ffe5f876540d7718b7b3be5b4391c490ec
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-02 09:16:46 +0800

    Time: 6 ms (17.98%) | Memory: 13.7 MB (60.67%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int m=mat.size(), n=mat[0].size();
-        unordered_map<int, priority_queue<int, vector<int>, greater<int>>>diagonals;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                diagonals[i-j].push(mat[i][j]);
+        int n = mat.size();
+        int m = mat[0].size();
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>>dia;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                dia[i-j].push(mat[i][j]);
             }
         }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                mat[i][j]=diagonals[i-j].top();
-                diagonals[i-j].pop();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m;j++){
+                mat[i][j] = dia[i-j].top();
+                dia[i-j].pop();
             }
         }
         return mat;
