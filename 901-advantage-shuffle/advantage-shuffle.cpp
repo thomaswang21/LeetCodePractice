aad54649c3ff275331bb160ee95396476dbfae44
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-02 13:33:16 +0800

    Time: 51 ms (38.21%) | Memory: 67.1 MB (80.62%) - LeetSync

--- 901-advantage-shuffle/advantage-shuffle.cpp
+++ 901-advantage-shuffle/advantage-shuffle.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        priority_queue<pair<int,int>>maxpq;
-        for(int i=0; i<n;i++){
-            maxpq.push({nums2[i],i});
+        int n = nums1.size();
+        priority_queue<pair<int,int>>pq;
+        for(int i =0; i < n; i++){
+            pq.push({nums2[i], i});
         }
-        sort(nums1.begin(),nums1.end());
-        int left=0, right=nums2.size()-1;
-        vector<int>res(n);
-        while(!maxpq.empty()){
-            auto pair=maxpq.top();
-            maxpq.pop();
-            int i=pair.second, maxval=pair.first;
-            if(maxval<nums1[right]){
-                res[i]=nums1[right];
+
+        sort(nums1.begin(), nums1.end());
+        int left = 0, right = nums2.size()-1;
+        vector<int> res(n);
+        while(!pq.empty()){
+            auto pair = pq.top();
+            pq.pop();
+            int i = pair.second , max = pair.first;
+            if(max<nums1[right]){
+                res[i] = nums1[right];
                 right--;
             }else{
-                res[i]=nums1[left];
+                res[i] = nums1[left];
                 left++;
             }
         }
