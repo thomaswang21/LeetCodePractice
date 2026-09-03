c10777f038b102481e4bc16b20ddf425377e646f
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-03 20:21:15 +0800

    Time: 3 ms (40.71%) | Memory: 8.6 MB (13.65%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -4,27 +4,16 @@ public:
         int i = 0;
         int j = 0;
         string s = "";
-        
-        // 阶段 1：只要两个字符串都还没遍历完，就交替添加
-        while (i < word1.size() && j < word2.size()) {
-            s += word1[i];
-            i++;
-            s += word2[j];
-            j++;
+        while(i < word1.size() || j < word2.size()){
+            if(i < word1.size()){
+                s += word1[i];
+                i++;
+            }
+            if(j < word2.size()){
+                s += word2[j];
+                j++;
+            }
         }
-        
-        // 阶段 2：如果 word1 还有剩下的，一口气加完
-        while (i < word1.size()) {
-            s += word1[i];
-            i++;
-        }
-        
-        // 阶段 3：如果 word2 还有剩下的，一口气加完
-        while (j < word2.size()) {
-            s += word2[j];
-            j++;
-        }
-        
         return s;
     }
 };
