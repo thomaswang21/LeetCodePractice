d86e88304316a03194065ff6991ea02bda0b41f7
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-03 10:00:36 +0800

    Time: 0 ms (100.00%) | Memory: 24 MB (61.42%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,22 +1,16 @@
 class NumArray {
-private:
+public:
     vector<int> prefixSums;
 
-public:
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        // Create an array of size n + 1 initialized to 0.
-        // prefixSums[i] will store the sum of elements from nums[0] to nums[i - 1].
-        prefixSums.resize(n + 1, 0);
-        
-        for (int i = 0; i < n; i++) {
-            prefixSums[i + 1] = prefixSums[i] + nums[i];
+        prefixSums.resize(n+1, 0);
+        for(int i = 0; i < nums.size(); i++){
+            prefixSums[i+1] = prefixSums[i] + nums[i];
         }
     }
     
     int sumRange(int left, int right) {
-        // The sum from index 'left' to 'right' is the sum up to 'right' 
-        // minus the sum up to 'left - 1'.
         return prefixSums[right + 1] - prefixSums[left];
     }
 };
