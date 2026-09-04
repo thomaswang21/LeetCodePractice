0a7c52c32a3712ba4d7b7affc60871659583ca91
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-04 20:09:23 +0800

    Time: 0 ms (100.00%) | Memory: 23.9 MB (61.35%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -4,13 +4,17 @@ public:
 
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        prefixSums.resize(n+1, 0);
-        for(int i = 0; i < nums.size(); i++){
-            prefixSums[i+1] = prefixSums[i] + nums[i];
+        prefixSums.resize(n + 1, 0);
+        
+        // 修正 1: 循环要走到 n，所以用 <= nums.size() 或者 i < n + 1
+        for(int i = 1; i <= nums.size(); i++){
+            prefixSums[i] = prefixSums[i-1] + nums[i-1];
         }
     }
     
     int sumRange(int left, int right) {
+        // 修正 2: 因为我们在 prefixSums 前面垫了一个 0，所以所有的查询索引都要 +1
+        // 这样即使 left 是 0，对应取的也是 prefixSums[0]，不会越界
         return prefixSums[right + 1] - prefixSums[left];
     }
 };
