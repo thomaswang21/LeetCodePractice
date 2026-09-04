fe3d1e5691daa87a6f951c8c5d55bc58b1c8d1ea
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-04 14:21:18 +0800

    Time: 47 ms (41.55%) | Memory: 67.3 MB (50.94%) - LeetSync

--- 901-advantage-shuffle/advantage-shuffle.cpp
+++ 901-advantage-shuffle/advantage-shuffle.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        priority_queue<pair<int,int>>pq;
-        for(int i =0; i < n; i++){
-            pq.push({nums2[i], i});
-        }
-
         sort(nums1.begin(), nums1.end());
-        int left = 0, right = nums2.size()-1;
-        vector<int> res(n);
+        vector<int> res(nums1.size());
+        priority_queue<pair<int,int>>pq;
+        for(int i =0; i < nums2.size(); i++){
+            pq.push({nums2[i],i});
+        }
+        int left = 0, right = nums1.size()-1;
         while(!pq.empty()){
             auto pair = pq.top();
             pq.pop();
-            int i = pair.second , max = pair.first;
-            if(max<nums1[right]){
+            int i = pair. second, max = pair.first;
+            if(max < nums1[right]){
                 res[i] = nums1[right];
                 right--;
             }else{
