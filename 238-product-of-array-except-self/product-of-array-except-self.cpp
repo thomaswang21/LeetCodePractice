3476b28e4a422dccdade22b49bdf23be120bc878
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-05 20:54:42 +0800

    Time: 0 ms (100.00%) | Memory: 40.1 MB (95.76%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,21 +3,17 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> answer(n, 1);
-        
-        // Pass 1: Calculate left products and store in answer
         int left_product = 1;
-        for (int i = 0; i < n; ++i) {
+        for(int i = 0; i < n; i++){
             answer[i] = left_product;
-            left_product *= nums[i]; // Update the running left product
+            left_product *= nums[i];
         }
-        
-        // Pass 2: Calculate right products on the fly and multiply with left products
         int right_product = 1;
-        for (int i = n - 1; i >= 0; --i) {
+        for(int i = n - 1; i >= 0; i--){
             answer[i] *= right_product;
-            right_product *= nums[i]; // Update the running right product
+            right_product *= nums[i];
+
         }
-        
         return answer;
     }
 };
