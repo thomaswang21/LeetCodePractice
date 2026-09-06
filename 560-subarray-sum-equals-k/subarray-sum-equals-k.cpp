8f11cb6d4ae8afbe6aa24b5a6e7af1a087a9c4ae
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-06 11:18:12 +0800

    Time: 31 ms (95.55%) | Memory: 45.5 MB (55.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,19 +1,32 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int>preSum(n+1,0);
-        for(int i=1; i<=n;i++){
-            preSum[i]=preSum[i-1]+nums[i-1];
-
+        // Map to store the frequency of prefix sums
+        unordered_map<int, int> prefix_counts;
+        
+        // Base case: a prefix sum of 0 has occurred exactly once (an empty prefix)
+        prefix_counts[0] = 1;
+        
+        int count = 0;
+        int curr_sum = 0;
+        
+        for (int num : nums) {
+            curr_sum += num;
+            
+            // If (curr_sum - k) exists, it means we found subarray(s) summing to k
+            if (prefix_counts.find(curr_sum - k) != prefix_counts.end()) {
+                count += prefix_counts[curr_sum - k];
+            }
+            
+            // Add the current prefix sum to our map for future calculations
+            prefix_counts[curr_sum]++;
         }
-        unordered_map<int,int>hash;
-        int res=0;
-        hash[0]=1;
-        for(int i=1;i<=n;i++){
-            res+=hash[preSum[i]-k];
-            hash[preSum[i]]++;
-        }
-        return res;
+        
+        return count;
     }
 };
