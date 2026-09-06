fc6955227d86a673bcc2fc05f58e1e5139a653db
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-06 10:58:47 +0800

    Time: 84 ms (21.36%) | Memory: 145.7 MB (99.67%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,30 +1,35 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> preSum(n + 1, 0);
-        for (int i = 1; i <= n; i++) {
-            preSum[i] = preSum[i - 1] + nums[i - 1];
-        }
-        unordered_map<int, int> valToIndex;
-        for (int i = 0; i < preSum.size(); i++) {
-            int val = preSum[i] % k;
-            if (!valToIndex.count(val)) {
-                valToIndex[val] = i;
-            }
-        }
-        for (int i = 1; i <preSum.size(); i++) {
-            int need = preSum[i] % k;
-            if (valToIndex.count(need)) {
-                if (i - valToIndex[need] >= 2) {
+        // Map to store {prefix_sum % k : index}
+        unordered_map<int, int> remainderMap;
+        
+        // Base case: To handle valid subarrays that start from the very beginning (index 0).
+        // A remainder of 0 at index -1 gives us a length of 2 if we hit a remainder of 0 at index 1.
+        remainderMap[0] = -1;
+        
+        int prefixSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
+            
+            // Calculate remainder
+            int remainder = prefixSum % k;
+            
+            // Have we seen this remainder before?
+            if (remainderMap.find(remainder) != remainderMap.end()) {
+                // Ensure the subarray length is at least 2
+                if (i - remainderMap[remainder] >= 2) {
                     return true;
                 }
+            } else {
+                // We only store the *first* time we see a remainder.
+                // We want to maximize the distance between matching remainders 
+                // to ensure the length is at least 2.
+                remainderMap[remainder] = i;
             }
         }
+        
         return false;
     }
 };
