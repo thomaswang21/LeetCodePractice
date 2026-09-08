be369f7bd2516e3b8ab5a8f3b31985a98592f2af
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-08 08:49:56 +0800

    Time: 11 ms (28.21%) | Memory: 10.9 MB (71.38%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,36 +1,23 @@
-#include <unordered_map>
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
-    int characterReplacement(std::string s, int k) {
-        // 使用哈希表记录窗口内每个字符的出现次数
-        std::unordered_map<char, int> countMap;
-        
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> countMap;
         int left = 0;
-        int maxCount = 0; // 当前窗口内单个字符出现的最大频次
+        int maxCount = 0;
         int maxLength = 0;
-
-        for (int right = 0; right < s.length(); ++right) {
+        
+        for(int right = 0; right < s.size(); right++){
             char rightChar = s[right];
             countMap[rightChar]++;
-            
-            // 更新当前窗口内字符的最大频次
-            maxCount = std::max(maxCount, countMap[rightChar]);
+            maxCount = max(maxCount, countMap[rightChar]);
 
-            // 如果当前窗口大小减去最多字符的频次大于 k，说明需要替换的字符过多
-            // 此时需要收缩左边界
-            while ((right - left + 1) - maxCount > k) {
+            while((right - left +1 ) - maxCount > k){
                 char leftChar = s[left];
                 countMap[leftChar]--;
                 left++;
             }
-
-            // 更新最大合法窗口长度
-            maxLength = std::max(maxLength, right - left + 1);
+            maxLength = max(maxLength, right - left +1);
         }
-
         return maxLength;
     }
 };
