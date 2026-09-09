7f0dbfcab20ea9c5aab5a6b14221dbd76f6920d0
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-09 20:17:22 +0800

    Time: 7 ms (49.57%) | Memory: 11.6 MB (37.72%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char,int>cnt;
-        for(auto c:p)cnt[c]++;
-        vector<int>res;
-        int tot=cnt.size();
-        int satisfy=0;
-        for(int i=0, j=0;i< s.size();i++){
-            cnt[s[i]]--;
-            if(cnt[s[i]]==0)satisfy++;
-            while(i-j+1>p.size()){
-                if(cnt[s[j]]==0)satisfy--;
-                cnt[s[j]]++;
-                j++;
+        vector<int> res;
+        // 注意：这里比较的是长度，而不是直接用 s > p 比较字典序
+        if(p.size() > s.size()){
+            return res;
+        }
+        
+        unordered_map<char, int> need;
+        unordered_map<char, int> window;
+        
+        // 必须先把目标字符串 p 的字符频次存入 need
+        for(char c : p){
+            need[c]++;
+        }
+        
+        int left = 0;
+        int right = 0;
+        int valid = 0;
+        
+        while(right < s.size()){
+            char c = s[right];
+            right++;
+            if(need.count(c)){
+                window[c]++;
+                if(window[c] == need[c]){
+                    valid++;
+                }
             }
-            if(satisfy==tot){
-                res.push_back(j);
+            
+            // 当窗口大小大于等于 p 的长度时，说明需要收缩窗口了
+            while(right - left >= p.size()){
+                // 窗口内的字符完全匹配 p 的字符频次
+                if(valid == need.size()){
+                    // 这里不直接 return true，而是把起点索引加入结果数组
+                    res.push_back(left); 
+                }
+                char d = s[left];
+                left++;
+                if(need.count(d)){
+                    if(window[d] == need[d]){
+                        valid--;
+                    }
+                    window[d]--;
+                }
             }
         }
         return res;
