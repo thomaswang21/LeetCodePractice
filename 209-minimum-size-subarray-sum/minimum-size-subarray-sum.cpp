0d7cd5c8e4fa99e9002deb7d52ee3fb49b1ee8a5
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-11 19:54:54 +0800

    Time: 0 ms (100.00%) | Memory: 41.8 MB (74.04%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
-        int min_length = INT_MAX;
+        int currentSum =0 ;
         int left = 0;
-        int current_sum = 0;
-        
-        for (int right = 0; right < n; ++right) {
-            current_sum += nums[right];
-            
-            // 当子数组的和大于等于目标值时，尝试从左侧缩小窗口
-            while (current_sum >= target) {
+        int min_length =INT_MAX;
+        for(int right = 0; right < n ; right++){
+            currentSum += nums[right];
+            while(currentSum >= target){
                 min_length = min(min_length, right - left + 1);
-                current_sum -= nums[left];
+                currentSum -= nums[left];
                 left++;
             }
         }
-        
-        // 使用 if-else 替代三元运算符
-        if (min_length == INT_MAX) {
+        if(min_length == INT_MAX){
             return 0;
         }
-        
         return min_length;
     }
 };
