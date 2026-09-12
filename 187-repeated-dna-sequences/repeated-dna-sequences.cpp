27fa8dc7ac8729fd8fe55e1890a180ec970ee1f5
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-12 10:25:48 +0800

    Time: 1260 ms (32.70%) | Memory: 375.7 MB (37.65%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        // Edge case: if the string is shorter than 10 chars, no 10-letter sequences exist
-        if (s.length() < 10) return {};
-        
+        if(s.length() < 10) return {};
         unordered_set<string> seen;
         unordered_set<string> repeated;
-        
-        // Slide a window of exactly length 10 across the string
-        for (int i = 0; i <= s.length() - 9 ; i++) {
-            string current_seq = s.substr(i, 10);
-            
-            // If it's already in 'seen', we found a duplicate
-            if (seen.count(current_seq)) {
-                repeated.insert(current_seq);
-            } else {
-                seen.insert(current_seq);
+
+        for(int i = 0; i <= s.length() -10; i++){
+            string current = s.substr(i, 10);
+            if(seen.count(current)){
+                repeated.insert(current);
+            }else{
+                seen.insert(current);
             }
         }
-        
-        // Return the collected repeated sequences as a vector
         return vector<string>(repeated.begin(), repeated.end());
     }
 };
