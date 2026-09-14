1171e84c36de3434e95cb260eed5c700d62f7e3a
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-14 13:25:34 +0800

    Time: 15 ms (17.10%) | Memory: 11 MB (41.57%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         unordered_map<char, int> countMap;
+        int maxd = 0;
+        int res = 0;
         int left = 0;
-        int maxCount = 0;
-        int maxLength = 0;
-        
         for(int right = 0; right < s.size(); right++){
-            char rightChar = s[right];
-            countMap[rightChar]++;
-            maxCount = max(maxCount, countMap[rightChar]);
-
-            while((right - left +1 ) - maxCount > k){
-                char leftChar = s[left];
-                countMap[leftChar]--;
+            char c = s[right];
+            countMap[c]++;
+            maxd = max(maxd, countMap[c]);
+            while((right - left +1) - maxd > k){
+                char l = s[left];
+                countMap[l]--;
                 left++;
             }
-            maxLength = max(maxLength, right - left +1);
+            res = max(res, right - left + 1);
         }
-        return maxLength;
+        return res;
     }
 };
