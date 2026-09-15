58d8e5425e20cd533821464202079b1afcecf1d5
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-15 12:41:49 +0800

    Time: 113 ms (50.10%) | Memory: 142.1 MB (20.18%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int , int>hash;
+        unordered_map<int, int> hash;
         for(int i = 0; i < nums.size(); i++){
-            if(hash.count(nums[i]) && i - hash[nums[i]] <= k){
+            if(hash.count(nums[i])&& (i - hash[nums[i]]) <= k){
                 return true;
+            }else{
+                hash[nums[i]] = i;
             }
-            hash[nums[i]] = i;
         }
         return false;
     }
