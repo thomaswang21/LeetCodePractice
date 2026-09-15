67e5847973ca923d98e0329c884cde794cb493d5
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-16 07:53:50 +0800

    Time: 57 ms (42.92%) | Memory: 19.4 MB (39.30%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> hash;
         int left = 0;
-        int maxLength = 0;
+        unordered_map<char, int>hash;
+        int maxL = 0;
         for(int right = 0; right < s.size(); right++){
-            // 发现重复字符，且该字符在当前窗口内，才更新 left
-            while(hash.count(s[right]) && hash[s[right]] >= left){
-                left = hash[s[right]] + 1;
+            if(hash.count(s[right]) && hash[s[right]] >= left){
+                left = hash[s[right]] +1;
             }
-            hash[s[right]] = right; // 更新字符最新的下标
-            maxLength = max(maxLength, right - left + 1);
+            hash[s[right]] = right;
+        maxL = max(maxL, right - left +1 );
         }
-        return maxLength;
+        return maxL;
     }
 };
