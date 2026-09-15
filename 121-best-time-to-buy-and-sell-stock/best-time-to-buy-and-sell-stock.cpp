5f4fc3d509ae7768e08a4621eaa3f5e8f07b08b5
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-15 12:50:11 +0800

    Time: 0 ms (100.00%) | Memory: 97.4 MB (26.26%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,26 +1,14 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
-
-using namespace std;
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_price = INT_MAX;
-        int max_profit = 0;
-        
-        for (int price : prices) {
-            // Update the lowest price seen so far
-            if (price < min_price) {
-                min_price = price;
-            } 
-            // Calculate profit if we sold today, and update max_profit if it's higher
-            else if (price - min_price > max_profit) {
-                max_profit = price - min_price;
+        int res = 0;
+        int minP = INT_MAX; // 将初始最小值设为最大整数
+        for(int i = 0; i < prices.size(); i++){
+            if(minP > prices[i]){
+                minP = prices[i];
             }
+            res = max(res, prices[i] - minP);
         }
-        
-        return max_profit;
+        return res;
     }
 };
