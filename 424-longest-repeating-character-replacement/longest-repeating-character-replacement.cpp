c78bdbc92a38bf4fe69ab298f174e00bf933d553
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-16 07:42:09 +0800

    Time: 3 ms (67.21%) | Memory: 10.9 MB (41.53%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -6,12 +6,12 @@ public:
         int maxd = 0;
         int res = 0;
         for(int right = 0; right < s.size(); right++){
-            char c = s[right];
-            hash[c]++;
-            maxd = max(maxd, hash[c]);
-           if((right - left +1) - maxd > k){
-                char l = s[left];
-                hash[l]--;
+           
+            hash[s[right]]++;
+            maxd = max(maxd, hash[s[right]]);
+           while((right - left +1) - maxd > k){
+                
+                hash[s[left]]--;
                 left++;
             }
             res = max(res, right - left + 1);
