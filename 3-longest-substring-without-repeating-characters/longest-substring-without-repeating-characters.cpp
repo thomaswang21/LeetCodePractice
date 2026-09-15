8df4bf502938809f101291f4f806d09ad392f2c7
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-15 13:08:27 +0800

    Time: 46 ms (51.00%) | Memory: 19.2 MB (48.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,12 +5,12 @@ public:
         int left = 0;
         int maxLength = 0;
         for(int right = 0; right < s.size(); right++){
-            while(hash.count(s[right]) && hash[s[right]] >= left){
+            // 发现重复字符，且该字符在当前窗口内，才更新 left
+            if(hash.count(s[right]) && hash[s[right]] >= left){
                 left = hash[s[right]] + 1;
             }
-            hash[s[right]] = right;
-            maxLength = max(maxLength, right - left +1);
-
+            hash[s[right]] = right; // 更新字符最新的下标
+            maxLength = max(maxLength, right - left + 1);
         }
         return maxLength;
     }
