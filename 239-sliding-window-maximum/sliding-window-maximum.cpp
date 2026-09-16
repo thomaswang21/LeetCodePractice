8f058ebb5c886893e173b15e9ef0c362e708e0eb
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-16 10:52:26 +0800

    Time: 20 ms (69.39%) | Memory: 134.4 MB (98.21%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,38 +1,28 @@
-#include <vector>
-#include <deque>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> result;
-        deque<int> dq; // 双端队列，存储的是元素的索引，而不是具体的值
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            // 1. 如果队首的索引已经超出了当前滑动窗口的范围，则将其移除
-            while (!dq.empty() && dq.front() < i - k + 1) {
-                dq.pop_front();
+        int n = nums.size();
+        vector<int> output(n - k + 1);
+        deque<int> q;
+        int l = 0, r = 0;
+
+        while (r < n) {
+            while (!q.empty() && nums[q.back()] < nums[r]) {
+                q.pop_back();
             }
-            
-            // 2. 保持队列的单调递减性质：
-            // 从队尾依次移除所有小于等于当前元素 nums[i] 的索引。
-            // 因为当前元素不仅更大，而且在窗口中存留的时间更长，
-            // 之前那些较小的元素绝对不可能成为现在或未来的最大值。
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
+            q.push_back(r);
+
+            if (l > q.front()) {
+                q.pop_front();
             }
-            
-            // 3. 将当前元素的索引加入队尾
-            dq.push_back(i);
-            
-            // 4. 当遍历的元素数量达到窗口大小 k 时（即 i >= k - 1），
-            // 队首元素对应的就是当前窗口的最大值，将其加入结果数组
-            if (i >= k - 1) {
-                result.push_back(nums[dq.front()]);
+
+            if ((r + 1) >= k) {
+                output[l] = nums[q.front()];
+                l++;
             }
+            r++;
         }
-        
-        return result;
+
+        return output;
     }
 };
