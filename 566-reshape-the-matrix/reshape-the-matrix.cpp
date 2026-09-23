3ba128fa5593e9734b80b29512cc137c2be0c401
Taoning21 <146942172+thomaswang21@users.noreply.github.com>
2026-09-23 15:55:50 +0800

    Time: 0 ms (100.00%) | Memory: 15 MB (80.07%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -3,19 +3,11 @@ public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         int m = mat.size();
         int n = mat[0].size();
-        
-        // Check if reshape is possible and legal
-        if (m * n != r * c) {
-            return mat;
+        if(m * n != r * c)return mat;
+        vector<vector<int>>reshaped(r, vector<int>(c));
+        for(int i = 0; i < m * n; i++){
+            reshaped[i / c][i % c] = mat[ i / n][i % n];
         }
-        
-        vector<vector<int>> reshaped(r, vector<int>(c));
-        
-        // Map 2D array to 1D and back to the new 2D array
-        for (int i = 0; i < m * n; ++i) {
-            reshaped[i / c][i % c] = mat[i / n][i % n];
-        }
-        
         return reshaped;
     }
 };
